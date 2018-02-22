89c6901f3189c8bd8b0087e8b37efe65d7c7fcf7
Ozee31 <flavien@commit42.fr>
2018-02-22 11:29:49 +0100

    add fontawesome #11032

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import { Alert } from "reactstrap";
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/fontawesome-pro-solid";
 import "./home.css";
 
 class Home extends Component {
   render() {
     return (
       <div className="home-page">
-        <Alert color="primary">Hey {this.props.user.username}</Alert>
+        <Alert color="primary">
+          <FontAwesomeIcon icon={faCoffee} />Hey {this.props.user.username}
+        </Alert>
 
         <section className="home-section">
           <button onClick={this.props.logout}>Se déconnecter</button>

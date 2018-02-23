de5b6bbe7a6cbb8294f5ec8abe56582d6a0912af
Ozee31 <flavien@commit42.fr>
2018-02-23 17:48:13 +0100

    searchResults : display search terms #11027

--- client/src/components/SearchResults/Terms/Terms.js
+++ client/src/components/SearchResults/Terms/Terms.js
@@ -3,9 +3,23 @@ import React from "react";
 class Terms extends React.Component {
   render() {
     return (
-      <dl className="row">
+      <dl className="terms-list row bg-light">
         <dt className="col-md-3">Raison Sociale</dt>
-        <dd className="col-md-9">{this.props.terms.raisonSociale}</dd>
+        <dd className="definition col-md-9">
+          {this.props.terms.raisonSociale}
+        </dd>
+
+        <dt className="col-md-3">Code NAF</dt>
+        <dd className="definition col-md-9">{this.props.terms.naf}</dd>
+
+        <dt className="col-md-3">Commune</dt>
+        <dd className="definition col-md-9">{this.props.terms.commune}</dd>
+
+        <dt className="col-md-3">Code Postal</dt>
+        <dd className="definition col-md-9">{this.props.terms.codePostal}</dd>
+
+        <dt className="col-md-3">Département</dt>
+        <dd className="definition col-md-9">{this.props.terms.departement}</dd>
       </dl>
     );
   }

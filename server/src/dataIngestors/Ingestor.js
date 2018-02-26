af79aedc67db2758132e16bb1d1517f852cec83a
Maxime <contact@maximepawlak.fr>
2018-02-26 14:19:28 +0100

    Add NomenclaturesIngestor.

--- server/src/dataIngestors/Ingestor.js
+++ server/src/dataIngestors/Ingestor.js
@@ -3,7 +3,10 @@ const XLSX = require("xlsx");
 class Ingestor{
   constructor(filePath, sheetName){
     const workbook = XLSX.readFile(filePath);
-    this.workSheet = workbook.Sheets[sheetName];
+    this.workbook = workbook;
+    if(sheetName){
+      this.workSheet = workbook.Sheets[sheetName];
+    }
   }
 
   save(){
---
+++ server/src/dataIngestors/NomenclaturesIngestor.js
@@ -0,0 +1,52 @@
+const Ingestor = require("./Ingestor");
+const WorkbookHelper = require("../helpers/WorkbookHelper");
+
+class NomenclaturesIngestor extends Ingestor {
+  constructor(filePath) {
+    super(filePath);
+  }
+
+  getNomenclatures() {
+    const wbh = new WorkbookHelper(this.workbook);
+
+    const sheetsParams = {
+      Code_activite_NAF: {
+        columnsToKeep: {
+          A: "code",
+          B: "libelle"
+        }
+      },
+      Code_Qualite_siege_2: {
+        columnsToKeep: {
+          A: "code",
+          B: "libelle"
+        }
+      },
+      Code_Qualite_siege: {
+        columnsToKeep: {
+          A: "code",
+          B: "libelle"
+        }
+      },
+      "Source_dernier_eff_phy ": {
+        columnsToKeep: {
+          A: "code",
+          B: "libelle_court",
+          C: "libelle"
+        }
+      },
+      Code_CJ3: {
+        columnsToKeep: {
+          A: "code",
+          B: "libelle_CJ3",
+          C: "libelle_CJ1"
+        }
+      }
+    };
+
+    const sheetsData = wbh.getSheetsData(sheetsParams);
+    return sheetsData;
+  }
+}
+
+module.exports = NomenclaturesIngestor;

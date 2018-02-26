62d25f8d89c62ce6d2e1ff8f03a0360bc525bc3d
Maxime <contact@maximepawlak.fr>
2018-02-26 17:40:22 +0100

    Add mongoose ref to index

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,9 @@
 var express = require('express')
 var app = express()
+var mongoose = require('mongoose');
+
+//DB setup
+mongoose.connect("mongodb://mongo:27017");
 
 app.get('/', function (req, res) {
   res.send('Hello World')

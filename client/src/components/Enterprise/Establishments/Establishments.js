71481ff71bdbc013eb0e87549862af93906bf2ac
Ozee31 <flavien@commit42.fr>
2018-02-28 16:14:58 +0100

    aside #11028

--- client/src/components/Enterprise/Establishments/Establishments.js
+++ client/src/components/Enterprise/Establishments/Establishments.js
@@ -1,27 +1,34 @@
 import React from "react";
 import Establishment from "./Establishment";
+import Value from "../../../elements/Value";
 
 class Establishments extends React.Component {
   render() {
-    let establishments = this.props.establishments.map(
-      (establishment, index) => (
-        <article key={index}>
-          <Establishment establishment={establishment} />
-        </article>
-      )
-    );
+    const { establishments, enterprise, headOffice } = this.props;
+
+    let establishmentsItems = establishments.map((establishment, index) => (
+      <article key={index}>
+        <Establishment establishment={establishment} />
+      </article>
+    ));
 
     return (
       <aside className="aside-contain establishments-aside col-md-3">
         <section>
-          <h1 className="h5">Entreprise [name]</h1>
+          <h1 className="h5">
+            Entreprise <Value value={enterprise.raison_sociale} empty="-" />
+          </h1>
 
           <dl className="row">
             <dt className="col-md-4">Raison Sociale</dt>
-            <dd className="definition col-md-8">[Raison Sociale]</dd>
+            <dd className="definition col-md-8">
+              <Value value={enterprise.raison_sociale} empty="-" />
+            </dd>
 
             <dt className="col-md-4">SIREN</dt>
-            <dd className="definition col-md-8">[SIREN]</dd>
+            <dd className="definition col-md-8">
+              <Value value={enterprise.siren} empty="-" />
+            </dd>
           </dl>
         </section>
 
@@ -30,7 +37,7 @@ class Establishments extends React.Component {
         <section>
           <h1 className="h5">Établissement siège / principal</h1>
 
-          <Establishment establishment={this.props.headOffice} />
+          <Establishment establishment={headOffice} />
         </section>
 
         <hr />
@@ -38,7 +45,7 @@ class Establishments extends React.Component {
         <section>
           <h1 className="h5">Établissements en Occitanie</h1>
 
-          {establishments}
+          {establishmentsItems}
         </section>
       </aside>
     );

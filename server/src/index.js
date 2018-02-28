bc214ff27b705b6913256d3ddae5e7ed020ca5e9
Maxime <contact@maximepawlak.fr>
2018-02-28 14:15:19 +0100

    Add API folder.

--- server/src/index.js
+++ server/src/index.js
@@ -6,7 +6,7 @@ var frentreprise = require("frentreprise");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var config = require("config");
-var uploadRouter = require("./api/upload");
+var apiRouter = require("./api");
 
 //DB setup
 if (config.has("mongo")) {
@@ -28,42 +28,7 @@ const htdocs_path = path.resolve(__dirname, "./htdocs");
 console.log(`Serving files from: ${htdocs_path}`);
 app.use(express.static(htdocs_path));
 
-app.get("/api/search", function(req, res) {
-  const query = (req.query["q"] || "").trim();
-  const data = {
-    query: {
-      q: query,
-      isSIRET: frentreprise.isSIRET(query),
-      isSIREN: frentreprise.isSIREN(query)
-    }
-  };
-
-  const logError = err => {
-    console.error(err);
-    data.error = true;
-    try {
-      data.message = err.toString();
-    } catch (Exception) {}
-  };
-
-  let freCall;
-
-  if (data.query.isSIREN || data.query.isSIRET) {
-    freCall = frentreprise.getEntreprise(data.query.q).then(entreprise => {
-      data.results = [entreprise.export()];
-    }, logError);
-  } else {
-    freCall = frentreprise.search(data.query.q).then(results => {
-      data.results = results;
-    }, logError);
-  }
-
-  freCall.then(() => {
-    res.send(data);
-  });
-});
-
-app.use("/api", uploadRouter);
+app.use("/api", apiRouter);
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies

da08a661a612be165ac1fc9d42b98bad0fd037d4
Ozee31 <flavien@commit42.fr>
2018-03-01 15:17:21 +0100

    static admin form #11023

--- client/src/components/Admin/Admin.js
+++ client/src/components/Admin/Admin.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./admin.css";
-import { Row, Col, Form, FormGroup, Input, Button, Alert } from "reactstrap";
+import {
+  Row,
+  Col,
+  Form,
+  FormGroup,
+  Input,
+  Button,
+  Alert,
+  Label
+} from "reactstrap";
 import FontAwesomeIcon from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/fontawesome-pro-solid";
 
@@ -9,8 +18,72 @@ class Admin extends React.Component {
     return (
       <div className="app-admin">
         <Row className="justify-content-md-center">
-          <Col xl="6" md="8">
+          <Col md="8">
             <h1 className="title">Administration</h1>
+
+            <Form className="import-form bg-light" onSubmit={this.props.submit}>
+              {this.props.hasError ? (
+                <Alert color="danger">
+                  Impossible de sauvegarder les fichiers
+                </Alert>
+              ) : (
+                ""
+              )}
+
+              {this.props.success ? (
+                <Alert color="success">
+                  Les fichiers ont été importés avec succès
+                </Alert>
+              ) : (
+                ""
+              )}
+
+              <FormGroup row>
+                <Label for="siene" md={4}>
+                  Fichier base SIENE
+                </Label>
+                <Col md={8}>
+                  <Input type="file" name="file" id="siene" />
+                </Col>
+              </FormGroup>
+
+              <FormGroup row>
+                <Label for="wikit" md={4}>
+                  Fichier intéractions pole T (WIKIT)
+                </Label>
+                <Col md={8}>
+                  <Input type="file" name="file" id="wikit" />
+                </Col>
+              </FormGroup>
+
+              <FormGroup row>
+                <Label for="sorac" md={4}>
+                  Fichier intéractions pole C (SORA)
+                </Label>
+                <Col md={8}>
+                  <Input type="file" name="file" id="sorac" />
+                </Col>
+              </FormGroup>
+
+              <FormGroup row>
+                <Label for="sora3e" md={4}>
+                  Fichier intéractions pole 3E (SORA)
+                </Label>
+                <Col md={8}>
+                  <Input type="file" name="file" id="sora3e" />
+                </Col>
+              </FormGroup>
+
+              <div className="d-flex justify-content-center">
+                <Button color="primary">
+                  {this.props.loading ? (
+                    <FontAwesomeIcon icon={faSpinner} spin />
+                  ) : (
+                    "Importer"
+                  )}
+                </Button>
+              </div>
+            </Form>
           </Col>
         </Row>
       </div>

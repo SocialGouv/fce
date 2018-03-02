2b8a3053b8d4ee712cbf84769af7309b1cacd211
Ozee31 <flavien@commit42.fr>
2018-03-02 11:26:18 +0100

    display establishment Nom Prenom if not categorie_juridique #11025

--- client/src/components/Enterprise/Sections/EstablishmentIdentity.js
+++ client/src/components/Enterprise/Sections/EstablishmentIdentity.js
@@ -16,10 +16,23 @@ class EstablishmentIdentity extends React.Component {
             <Value value={establishment.enseigne} empty="-" />
           </dd>
 
-          <dt className="dt col-md-4">Raison Sociale</dt>
-          <dd className="dd definition col-md-8">
-            <Value value={enterprise.raison_sociale} empty="-" />
-          </dd>
+          {enterprise.categorie_juridique
+            ? [
+                <dt className="dt col-md-4">Raison Sociale</dt>,
+                <dd className="dd definition col-md-8">
+                  <Value value={enterprise.raison_sociale} empty="-" />
+                </dd>
+              ]
+            : [
+                <dt className="dt col-md-4">Nom</dt>,
+                <dd className="dd definition col-md-8">
+                  <Value value={enterprise.nom} empty="-" />
+                </dd>,
+                <dt className="dt col-md-4">Prenom</dt>,
+                <dd className="dd definition col-md-8">
+                  <Value value={enterprise.prenom} empty="-" />
+                </dd>
+              ]}
 
           <dt className="dt col-md-4">Nom commercial</dt>
           <dd className="dd definition col-md-8">

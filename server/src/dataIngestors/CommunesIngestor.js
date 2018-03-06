c01e8902e06d8dcd20ceb6fbd0f1f84bc7c6168e
Maxime <contact@maximepawlak.fr>
2018-03-06 11:20:36 +0100

    update Communes ingestor

--- server/src/dataIngestors/CommunesIngestor.js
+++ server/src/dataIngestors/CommunesIngestor.js
@@ -1,6 +1,7 @@
 const Ingestor = require("./Ingestor");
 const WorksheetHelper = require("../helpers/WorksheetHelper");
 const Commune = require("../models/CommuneModel");
+const Etablissement = require("../models/EtablissementModel");
 
 class CommunesIngestor extends Ingestor {
   constructor() {
@@ -8,26 +9,43 @@ class CommunesIngestor extends Ingestor {
     this.Model = Commune;
   }
 
-  getData(etablissements) {
-    return this.getCommunesFromEtablissements(etablissements);
+  getData(params) {
+    if (params && params.etablissements) {
+      return this.getCommunesFromEtablissements(params.etablissements);
+    } else if (params && params.mongo) {
+      return this.getCommunesFromMongo();
+    }
   }
 
-  getCommunesFromEtablissements(etablissements){
+  getCommunesFromEtablissements(etablissements) {
     let communes = [];
-    let codes = []
-    etablissements.map( etablissement => {
+    let codes = [];
+    etablissements.map(etablissement => {
       let commune = {
         libelle_commune: etablissement.libelle_commune,
-        code_commune: etablissement.code_commune,
+        code_commune: etablissement.code_commune
       };
-      if(!codes.includes(commune.code_commune) ){
+      if (!codes.includes(commune.code_commune)) {
         communes.push(commune);
         codes.push(commune.code_commune);
       }
-    })
+    });
     return communes;
   }
 
+  getCommunesFromMongo() {
+    let communes = [];
+    return Etablissement.findDisctinctCommunes().then(data => {
+      const codes = data;
+      codes.map(code => {
+        let commune = {
+          libelle_commune: code
+        };
+        communes.push(commune);
+      });
+      return communes;
+    });
+  }
 }
 
 module.exports = CommunesIngestor;
--- server/src/dataIngestors/CommunesIngestor.test.js
+++ server/src/dataIngestors/CommunesIngestor.test.js
@@ -3,6 +3,7 @@ require("../mongo/db");
 const EtablissementsIngestor = require("./EtablissementsIngestor");
 const CommunesIngestor = require("./CommunesIngestor");
 const Commune = require("../models/CommuneModel");
+const Etablissement = require("../models/EtablissementModel");
 
 const filePath = "./data/SIENE_test.csv";
 const TIMEOUT = 10000;
@@ -38,11 +39,49 @@ describe("save()", () => {
 
     const communesIngestor = new CommunesIngestor();
     const data = communesIngestor.getCommunesFromEtablissements(etablissements);
-    return communesIngestor.save(etablissements).then(data => {
+    const saveParams = {
+      etablissements
+    };
+    return communesIngestor.save(saveParams).then(data => {
       expect(data.length).toEqual(3);
       expect(data[0].code_commune).toEqual("30189");
       expect(data[1].code_commune).toEqual("31149");
       expect(data[2].code_commune).toEqual("31555");
     });
+  });
+});
+
+
+
+describe("getCommunesFromMongo()", () => {
+  beforeEach(() => {
+    return Commune.remove({}).then(() => {
+      Etablissement.remove({});
+    });
+  }, TIMEOUT);
+
+  afterEach(() => {
+    return Commune.remove({}).then(() => {
+      Etablissement.remove({});
+    });
+  }, TIMEOUT);
+
+  test("default", () => {
+    const etablissementsIngestor = new EtablissementsIngestor(filePath);
+
+    const communesIngestor = new CommunesIngestor();
+    const saveParams = {
+      mongo: true
+    };
+    return etablissementsIngestor
+      .save()
+      .then(data => {
+        return communesIngestor.getCommunesFromMongo();
+      })
+      .then(data => {
+        expect(data.length).toEqual(3);
+        expect(data[0].libelle_commune).toEqual("COLOMIERS");
+        expect(data[1].libelle_commune).toEqual("NIMES");
+      });
   });
 });

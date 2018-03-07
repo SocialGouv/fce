bf025e18cdd68a28ece66a9e18947552e8d50f47
Ozee31 <flavien@commit42.fr>
2018-03-07 16:47:11 +0100

    update establishment view (establishmentIdentity) #11029

--- client/src/components/Enterprise/Sections/EstablishmentIdentity.js
+++ client/src/components/Enterprise/Sections/EstablishmentIdentity.js
@@ -15,40 +15,6 @@ class EstablishmentIdentity extends React.Component {
             <Value value={establishment.enseigne} empty="-" />
           </dd>
 
-          {enterprise.categorie_juridique
-            ? [
-                <dt className="dt col-md-4" key={0}>
-                  Raison Sociale
-                </dt>,
-                <dd className="dd definition col-md-8" key={1}>
-                  <Value value={enterprise.raison_sociale} empty="-" />
-                </dd>
-              ]
-            : [
-                <dt className="dt col-md-4" key={0}>
-                  Nom
-                </dt>,
-                <dd className="dd definition col-md-8" key={1}>
-                  <Value value={enterprise.nom} empty="-" />
-                </dd>,
-                <dt className="dt col-md-4" key={2}>
-                  Prenom
-                </dt>,
-                <dd className="dd definition col-md-8" key={3}>
-                  <Value value={enterprise.prenom} empty="-" />
-                </dd>
-              ]}
-
-          <dt className="dt col-md-4">Nom commercial</dt>
-          <dd className="dd definition col-md-8">
-            <Value value={enterprise.nom_commercial} empty="-" />
-          </dd>
-
-          <dt className="dt col-md-4">SIREN</dt>
-          <dd className="dd col-md-8">
-            <Value value={enterprise.siren} empty="-" />
-          </dd>
-
           <dt className="dt col-md-4">SIRET</dt>
           <dd className="dd col-md-8">
             <Value value={establishment.siret} empty="-" />
@@ -57,11 +23,6 @@ class EstablishmentIdentity extends React.Component {
           <dt className="dt col-md-4">Catégorie d'établissement</dt>
           <dd className="dd definition col-md-8">
             <Value value={establishment.categorie_etablissement} empty="-" />
-          </dd>
-
-          <dt className="dt col-md-4">SIRET du siège social</dt>
-          <dd className="dd col-md-8">
-            <Value value={headOffice.siret} empty="-" />
           </dd>
 
           <dt className="dt col-md-4">Adresse</dt>

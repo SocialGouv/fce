ad36197c9c1a33b719c3d38eef6d8979c00149e7
Giovanni Olivera <giovanni@commit42.fr>
2018-03-08 12:33:16 +0100

    Remove service worker

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "./assets/stylesheets/styles.css";
 import App from "./components/App";
-import registerServiceWorker from "./registerServiceWorker";
 import smoothscroll from "smoothscroll-polyfill";
 
 smoothscroll.polyfill();

a8cab6608016bc07c21eed9f3ab18e0d1b509993
Giovanni Olivera <giovanni@commit42.fr>
2018-03-08 14:49:57 +0100

    Update dist docker

--- .c42/dist/run.sh
+++ .c42/dist/run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-docker build -t direccte . && docker run -p :80 --restart=always direccte
+mkdir -p data/db && \
+docker build -t direccte . && \
+(docker rm -f direccte; exit 0) && \
+docker run --name=direccte -d -p 80:80  -v data:/data --restart=always direccte && \
+docker logs --follow direccte

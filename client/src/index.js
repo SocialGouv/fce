09c07451bd9bca2a1aac3c215cbd90ff7ec0f930
Giovanni Olivera <giovanni@commit42.fr>
2018-03-14 11:02:34 +0100

    Removing service worker

--- client/src/index.js
+++ client/src/index.js
@@ -7,4 +7,20 @@ import smoothscroll from "smoothscroll-polyfill";
 
 smoothscroll.polyfill();
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then(workers => {
+    Array.isArray(workers) &&
+      (console.log(`Found ${workers.length} service workers...`) || true) &&
+      workers.forEach(
+        worker =>
+          worker &&
+          worker.unregister &&
+          console.log(
+            "Removing service worker :",
+            worker.unregister().then(console.log, console.error) && worker
+          )
+      );
+  }, console.error);
+}
+
 ReactDOM.render(<App />, document.getElementById("root"));

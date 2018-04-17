b0f8156eec4aaa8ed07f2953e6df58274286295f
Giovanni Olivera <giovanni@commit42.fr>
2018-04-17 11:42:26 +0200

    Build project in a separate folder

--- .c42/docker/builder/warmup.sh
+++ .c42/docker/builder/warmup.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
+set -e
 
 echo "#!/bin/sh" > /bin/ruby
-echo "exec docker run --rm -v \"\$PWD\":/usr/src/app -w /usr/src/app ruby:2.5 ruby \"\$@\"" > /bin/ruby
+echo "exec docker run --rm -v \"\`pwd\`\":/usr/src/app -w /usr/src/app ruby:2.5 ruby \"\$@\"" > /bin/ruby
 chmod a+x /bin/ruby
 
 echo "#!/bin/sh" > /bin/gem
-echo "exec docker run --rm -v \"\$PWD\":/usr/src/app -w /usr/src/app ruby:2.5 gem \"\$@\"" > /bin/gem
+echo "exec docker run --rm -v \"\`pwd\`\":/usr/src/app -w /usr/src/app ruby:2.5 gem \"\$@\"" > /bin/gem
 chmod a+x /bin/gem
 
 echo "#!/bin/sh" > /bin/bundle
-echo "exec docker run --rm -v \"\$PWD\":/usr/src/app -w /usr/src/app ruby:2.5 bundle \"\$@\"" > /bin/bundle
+echo "exec docker run --rm -v \"\`pwd\`\":/usr/src/app -w /usr/src/app ruby:2.5 bundle \"\$@\"" > /bin/bundle
 chmod a+x /bin/bundle
+
+rm -rf /build
+mkdir /build
+cp -Rf /project /build
+cd /build
 
 exec "$@"

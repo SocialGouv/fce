8a8615bf03fb2fc670615f82543de1c8ae964a1a
Ozee31 <flavien@commit42.fr>
2018-04-18 14:44:18 +0200

    replace all accessors component by component function #11161

--- client/src/components/SearchResults/SearchResults.js
+++ client/src/components/SearchResults/SearchResults.js
@@ -56,147 +56,131 @@ class SearchResults extends React.Component {
             {Array.isArray(this.props.results) && this.props.results.length ? (
               <ReactTable
                 data={this.props.results}
-                columns={[
-                  {
-                    Header: "SIRET",
-                    id: "siret",
-                    accessor: e => (
-                      <Value value={e.etablissement.siret} empty="-" />
-                    )
-                  },
-                  {
-                    Header: "SIREN",
-                    id: "siren",
-                    accessor: e => <Value value={e.siren} empty="-" />
-                  },
-                  {
-                    Header: "Raison Sociale / Nom",
-                    id: "nom",
-                    accessor: e => (
-                      <Value value={e.raison_sociale || e.nom} empty="-" />
-                    )
-                  },
-                  {
-                    Header: "État",
-                    id: "etat",
-                    accessor: e => (
-                      <Value
-                        value={
-                          e.etablissement.etat_etablissement &&
-                          e.etablissement.etat_etablissement.label
-                        }
-                        empty="-"
-                      />
-                    )
-                  },
-                  {
-                    Header: "Code postal",
-                    id: "code_postal",
-                    accessor: e => (
-                      <Value
-                        value={
-                          e.etablissement.adresse_components &&
-                          e.etablissement.adresse_components.code_postal &&
-                          `${
-                            e.etablissement.adresse_components.code_postal
-                          }\u00A0(${
-                            e.etablissement.adresse_components.localite
-                          })`
-                        }
-                        empty="-"
-                      />
-                    )
-                  },
-                  {
-                    Header: "Département",
-                    id: "departement",
-                    accessor: e => (
-                      <Value
-                        value={
-                          e.etablissement.adresse_components &&
-                          e.etablissement.adresse_components.code_postal &&
-                          e.etablissement.adresse_components.code_postal.substr(
-                            0,
-                            2
-                          )
-                        }
-                        empty="-"
-                      />
-                    )
-                  },
-                  {
-                    Header: "Activité",
-                    id: "activite",
-                    accessor: e => (
-                      <Value value={e.etablissement.activite} empty="-" />
-                    )
-                  },
-                  {
-                    Header: "Cat. Etablissement",
-                    id: "categorie_etablissement",
-                    accessor: e => (
-                      <Value
-                        value={e.etablissement.categorie_etablissement}
-                        empty="-"
-                      />
-                    )
-                  },
-                  {
-                    Header: "Interactions",
-                    id: "total-interactions",
-                    accessor: e => (
-                      <Value
-                        value={e.etablissement.totalInteractions}
-                        empty="-"
-                      />
-                    )
-                  },
-                  {
-                    Header: "Pole C",
-                    id: "pole-c",
-                    accessor: e => (
-                      <Value
-                        value={
-                          e.etablissement.interactions &&
-                          e.etablissement.interactions["C"]
-                        }
-                        empty="-"
-                      />
-                    )
-                  },
-                  {
-                    Header: "Pole 3E",
-                    id: "pole-3e",
-                    accessor: e => (
-                      <Value
-                        value={
-                          e.etablissement.interactions &&
-                          e.etablissement.interactions["3E"]
-                        }
-                        empty="-"
-                      />
-                    )
-                  },
-                  {
-                    Header: "Pole T",
-                    id: "pole-t",
-                    accessor: e => (
-                      <Value
-                        value={
-                          e.etablissement.interactions &&
-                          e.etablissement.interactions["T"]
-                        }
-                        empty="-"
-                      />
-                    )
-                  }
-                ]}
                 defaultPageSize={
                   this.props.results.length >= 25
                     ? 25
                     : this.props.results.length
                 }
                 className="-striped -highlight"
+                filterable={true}
+                columns={[
+                  {
+                    Header: "SIRET",
+                    id: "siret",
+                    accessor: e =>
+                      Value({ value: e.etablissement.siret, empty: "-" })
+                  },
+                  {
+                    Header: "SIREN",
+                    id: "siren",
+                    accessor: e => Value({ value: e.siren, empty: "-" })
+                  },
+                  {
+                    Header: "Raison Sociale / Nom",
+                    id: "nom",
+                    accessor: e =>
+                      Value({ value: e.raison_sociale || e.nom, empty: "-" })
+                  },
+                  {
+                    Header: "État",
+                    id: "etat",
+                    accessor: e =>
+                      Value({
+                        value:
+                          e.etablissement.etat_etablissement &&
+                          e.etablissement.etat_etablissement.label,
+                        empty: "-"
+                      })
+                  },
+                  {
+                    Header: "Code postal",
+                    id: "code_postal",
+                    accessor: e =>
+                      Value({
+                        value:
+                          e.etablissement.adresse_components &&
+                          e.etablissement.adresse_components.code_postal &&
+                          `${
+                            e.etablissement.adresse_components.code_postal
+                          }\u00A0(${
+                            e.etablissement.adresse_components.localite
+                          })`,
+                        empty: "-"
+                      })
+                  },
+                  {
+                    Header: "Département",
+                    id: "departement",
+                    accessor: e =>
+                      Value({
+                        value:
+                          e.etablissement.adresse_components &&
+                          e.etablissement.adresse_components.code_postal &&
+                          e.etablissement.adresse_components.code_postal.substr(
+                            0,
+                            2
+                          ),
+                        empty: "-"
+                      })
+                  },
+                  {
+                    Header: "Activité",
+                    id: "activite",
+                    accessor: e =>
+                      Value({ value: e.etablissement.activite, empty: "-" })
+                  },
+                  {
+                    Header: "Cat. Etablissement",
+                    id: "categorie_etablissement",
+                    accessor: e =>
+                      Value({
+                        value: e.etablissement.categorie_etablissement,
+                        empty: "-"
+                      })
+                  },
+                  {
+                    Header: "Interactions",
+                    id: "total-interactions",
+                    accessor: e =>
+                      Value({
+                        value: e.etablissement.totalInteractions,
+                        empty: ""
+                      })
+                  },
+                  {
+                    Header: "Pole C",
+                    id: "pole-c",
+                    accessor: e =>
+                      Value({
+                        value:
+                          e.etablissement.interactions &&
+                          e.etablissement.interactions["C"],
+                        empty: ""
+                      })
+                  },
+                  {
+                    Header: "Pole 3E",
+                    id: "pole-3e",
+                    accessor: e =>
+                      Value({
+                        value:
+                          e.etablissement.interactions &&
+                          e.etablissement.interactions["3E"],
+                        empty: ""
+                      })
+                  },
+                  {
+                    Header: "Pole T",
+                    id: "pole-t",
+                    accessor: e =>
+                      Value({
+                        value:
+                          e.etablissement.interactions &&
+                          e.etablissement.interactions["T"],
+                        empty: ""
+                      })
+                  }
+                ]}
               />
             ) : (
               ""

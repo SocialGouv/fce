4e615c1c45855ac7adbf0987e418592b99060dcd
Mathieu HAAGE <mathieucommit42.fr>
2018-10-08 11:36:34 +0200

    Script install.sh created - refs #11445

---
+++ .c42/scripts/install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+function dcRun
+{
+  CMD="docker-compose run --rm"
+  ARG="$1"
+  eval ${CMD} ${ARG}
+}
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd $DIR/../..
+
+echo
+echo "============================"
+echo "INSTALLATION OF THE PROJECT "
+echo "============================"
+
+#Install docker-compose.yml
+.c42/scripts/docker-install.sh
+
+echo
+echo "Install yarn"
+echo "------------"
+dcRun "front yarn install"
+dcRun "server yarn install"
+echo
+echo "Start docker"
+echo "------------"
+docker-compose up -d
+echo
+echo "Restart front"
+echo "-------------"
+docker-compose restart front
+echo
+echo "Restart server"
+echo "--------------"
+docker-compose restart server
+
+echo
+echo "Done!"
+echo

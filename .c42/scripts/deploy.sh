88a5cfcf407a995f02278ce4bc70ea1d97b192b3
Ozee31 <flavien@commit42.fr>
2019-01-10 09:43:56 +0100

    fix deploy.sh & Jenkinsfile

--- .c42/scripts/deploy.sh
+++ .c42/scripts/deploy.sh
@@ -11,24 +11,24 @@ KEEP_RELEASES=2
 echo "Creating folders"
 
 ssh $SSH_HOST 'bash -s' <<'CMD'
- mkdir -p ~/deployment/fce2/releases
+ mkdir -p ~/deployment/fce/releases
 CMD
 
 echo "Creating release"
-rsync -avzP dist/. $SSH_HOST:~/deployment/fce2/releases/$RELEASEN
+rsync -avzP dist/. $SSH_HOST:~/deployment/fce/releases/$RELEASEN
 
 ssh $SSH_HOST KEEP_RELEASES=$KEEP_RELEASES RELEASEN=$RELEASEN 'bash -s' <<'CMD'
 echo "Launching npm install"
-cd ~/deployment/fce2/releases/$RELEASEN && npm install
+cd ~/deployment/fce/releases/$RELEASEN && npm install
 
 echo "Linking release"
-ln -nfs ~/deployment/fce2/releases/$RELEASEN ~/deployment/fce2/current;
+ln -nfs ~/deployment/fce/releases/$RELEASEN ~/deployment/fce/current;
 
 echo "Killing old instance"
 ps -ef | grep node | grep "fce" | grep -v grep | awk "{print \$2}" | xargs kill -9
 
 echo "Cleaning old releases"
-cd ~/easymile/releases;
+cd ~/deployment/fce/releases;
 
 RELEASES=$(ls -d */ | sort -r);
 COUNT=$(echo "$RELEASES" | wc -l);

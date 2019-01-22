45c1e843eeea9ef8d096a49961e7449257345500
Ozee31 <flavien@commit42.fr>
2019-01-22 14:53:49 +0100

    test predecesseur to frentreprise / etablissement

--- src/frentreprise/src/DataSources/ApiGouv/EtablissementsAPI/predecesseur.js
+++ src/frentreprise/src/DataSources/ApiGouv/EtablissementsAPI/predecesseur.js
@@ -1,23 +1,25 @@
 import utils from "../../../Utils/utils";
 
 const predecesseur = async (SIRET, Axios, params) => {
-  return await utils.requestAPI(
-    Axios,
-    `etablissements/${SIRET}/predecesseur`,
-    params).then(data => {
+  return await utils
+    .requestAPI(Axios, `etablissements/${SIRET}/predecesseur`, params)
+    .then(data => {
       if (data && data.predecesseur) {
         const predecesseur = data.predecesseur;
 
         if (predecesseur) {
-          return { predecesseur: {
-            siret: predecesseur.predecesseur_siret,
-            date_transfert: utils.convertDate(
-              predecesseur.predecesseur_date_etablissement
-            )
-          }};
+          return {
+            predecesseur: {
+              siret: predecesseur.predecesseur_siret,
+              date_transfert: utils.convertDate(
+                predecesseur.predecesseur_date_etablissement
+              )
+            }
+          };
         }
       }
-    })
+      return {};
+    });
 };
 
 export default predecesseur;

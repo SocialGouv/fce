c45161667199e9546b7ac8c066228d850b72c0d9
Ozee31 <flavien@commit42.fr>
2019-01-23 15:09:48 +0100

    update front

--- src/client/src/components/Enterprise/Sections/EnterpriseIdentity.js
+++ src/client/src/components/Enterprise/Sections/EnterpriseIdentity.js
@@ -17,27 +17,27 @@ class EnterpriseIdentity extends React.Component {
 
           {enterprise.nom || enterprise.prenom
             ? [
-              <dt className="dt col-md-4" key="name_label">
-                Nom
-              </dt>,
-              <dd className="dd definition col-md-8" key="name_value">
-                <Value value={enterprise.nom} empty="-" />
-              </dd>,
-              <dt className="dt col-md-4" key="firstname_label">
-                Prenom
-              </dt>,
-              <dd className="dd definition col-md-8" key="firstname_value">
-                <Value value={enterprise.prenom} empty="-" />
-              </dd>
-            ] : [
+                <dt className="dt col-md-4" key="name_label">
+                  Nom
+                </dt>,
+                <dd className="dd definition col-md-8" key="name_value">
+                  <Value value={enterprise.nom} empty="-" />
+                </dd>,
+                <dt className="dt col-md-4" key="firstname_label">
+                  Prenom
+                </dt>,
+                <dd className="dd definition col-md-8" key="firstname_value">
+                  <Value value={enterprise.prenom} empty="-" />
+                </dd>
+              ]
+            : [
                 <dt className="dt col-md-4" key="rs_label">
                   Raison Sociale
                 </dt>,
                 <dd className="dd definition col-md-8" key="rs_value">
                   <Value value={enterprise.raison_sociale} empty="-" />
                 </dd>
-              ]
-            }
+              ]}
 
           <dt className="dt col-md-4">Nom commercial</dt>
           <dd className="dd definition col-md-8">
@@ -49,9 +49,29 @@ class EnterpriseIdentity extends React.Component {
             <Value value={enterprise.sigle} empty="-" />
           </dd>
 
+          <dt className="dt col-md-4">Catégorie</dt>
+          <dd className="dd definition col-md-8">
+            <Value value={enterprise.categorie_entreprise} empty="-" />
+          </dd>
+
           <dt className="dt col-md-4">Catégorie juridique</dt>
           <dd className="dd definition col-md-8">
             <Value value={enterprise.categorie_juridique} empty="-" />
+          </dd>
+
+          <dt className="dt col-md-4">Capital social</dt>
+          <dd className="dd definition col-md-8">
+            <Value value={enterprise.capital_social} empty="-" />
+          </dd>
+
+          <dt className="dt col-md-4">Forme juridique</dt>
+          <dd className="dd definition col-md-8">
+            <Value value={enterprise.forme_juridique} empty="-" />
+          </dd>
+
+          <dt className="dt col-md-4">Code forme juridique</dt>
+          <dd className="dd definition col-md-8">
+            <Value value={enterprise.forme_juridique_code} empty="-" />
           </dd>
         </dl>
       </section>

c07c62105ff49e95860a7fbe0ddcdd4c59ee4ece
Ozee31 <flavien@commit42.fr>
2019-01-25 15:26:06 +0100

    frentreprise : refactoring apigouv/getEntreprise

--- src/frentreprise/src/DataSources/ApiGouv/EntreprisesAPI/getEntreprise.js
+++ src/frentreprise/src/DataSources/ApiGouv/EntreprisesAPI/getEntreprise.js
@@ -1,70 +1,67 @@
 import utils from "../../../Utils/utils";
+import getData from "../../getData";
 
 const getEntreprise = async (SIREN, Axios, params) => {
   return await utils
     .requestAPI(Axios, `entreprises/${SIREN}`, params)
     .then(data => {
-      const out = {};
-      if (data && data.entreprise) {
-        const ent = data.entreprise;
+      const fields = [
+        "categorie_entreprise",
+        "siren",
+        "raison_sociale",
+        "nom_commercial",
+        "nom",
+        "prenom",
+        "siret_siege_social",
+        "capital_social",
+        "forme_juridique",
+        "forme_juridique_code",
+        { in: "naf_entreprise", out: "naf" },
+        { in: "libelle_naf_entreprise", out: "libelle_naf" },
+        {
+          in: "tranche_effectif_salarie_entreprise.date_reference",
+          out: "annee_tranche_effectif"
+        },
+        {
+          in: "tranche_effectif_salarie_entreprise.intitule",
+          out: "tranche_effectif"
+        },
+        {
+          in: "tranche_effectif_salarie_entreprise",
+          out: "entreprise_employeur",
+          callback: trancheEffectif => trancheEffectif && +trancheEffectif.a > 0
+        },
+        {
+          in: "mandataires_sociaux",
+          out: "mandataires_sociaux",
+          callback: mandataires => {
+            if (!Array.isArray(mandataires)) {
+              return null;
+            }
 
-        [
-          "categorie_entreprise",
-          "siren",
-          "raison_sociale",
-          "nom_commercial",
-          "nom",
-          "prenom",
-          "siret_siege_social",
-          "capital_social",
-          "forme_juridique",
-          "forme_juridique_code",
-          { in: "naf_entreprise", out: "naf" },
-          { in: "libelle_naf_entreprise", out: "libelle_naf" }
-        ].forEach(key => {
-          const inKey = typeof key === "object" ? key.in : key;
-          const outKey = typeof key === "object" ? key.out : key;
-          const value = ent[inKey];
-
-          if (typeof value === "boolean") out[outKey] = value;
-          else out[outKey] = value || undefined;
-        });
-
-        if (
-          ent.tranche_effectif_salarie_entreprise &&
-          typeof ent.tranche_effectif_salarie_entreprise === "object"
-        ) {
-          out.annee_tranche_effectif =
-            +ent.tranche_effectif_salarie_entreprise.date_reference ||
-            undefined;
-          out.tranche_effectif =
-            ent.tranche_effectif_salarie_entreprise.intitule || undefined;
-        }
-
-        out.entreprise_employeur = ent.tranche_effectif_salarie_entreprise
-          ? +ent.tranche_effectif_salarie_entreprise.a > 0
-          : undefined;
-
-        if (Array.isArray(ent.mandataires_sociaux)) {
-          out.mandataires_sociaux = [];
-          ent.mandataires_sociaux.forEach(manso => {
-            out.mandataires_sociaux.push({
-              nom: manso.nom,
-              prenom: manso.prenom,
-              fonction: manso.fonction,
-              raison_sociale: manso.raison_sociale
+            return mandataires.map(mandataire => {
+              return {
+                nom: mandataire.nom,
+                prenom: mandataire.prenom,
+                fonction: mandataire.fonction,
+                raison_sociale: mandataire.raison_sociale
+              };
             });
-          });
+          }
+        },
+        {
+          in: "date_creation",
+          out: "date_de_creation",
+          callback: date => utils.convertDate(date)
+        },
+        {
+          in: "date_radiation",
+          out: "date_de_radiation",
+          callback: date => utils.convertDate(date)
         }
+      ];
 
-        if (ent.date_creation) {
-          out.date_de_creation = utils.convertDate(ent.date_creation);
-        }
-        if (ent.date_radiation) {
-          out.date_de_radiation = utils.convertDate(ent.date_radiation);
-        }
-      }
-      return out;
+      return data && data.entreprise ? getData(data.entreprise, fields) : {};
     });
 };
 
---
+++ src/frentreprise/src/DataSources/getData.js
@@ -0,0 +1,23 @@
+import _get from "lodash.get";
+
+export default (data, fields) => {
+  const out = {};
+
+  fields.forEach(field => {
+    const inKey = typeof field === "object" ? field.in : field;
+    const outKey = typeof field === "object" ? field.out : field;
+    let value = _get(data, inKey);
+
+    if (field.callback) {
+      value = field.callback(value);
+    }
+
+    if (typeof value === "boolean") {
+      out[outKey] = value;
+    } else {
+      out[outKey] = value || undefined;
+    }
+  });
+
+  return out;
+};

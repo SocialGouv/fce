fe0f618ec3476ead852539a88f069c91524a2aab
Ozee31 <flavien@commit42.fr>
2019-02-22 10:31:57 +0100

    [client] add etat_etablissement & remove activite to SearchResults

--- src/client/src/components/SearchResults/SearchResults.js
+++ src/client/src/components/SearchResults/SearchResults.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Row, Col, Button, Alert } from "reactstrap";
 import FontAwesomeIcon from "@fortawesome/react-fontawesome";
-import { faFileExcel, faPrint } from "@fortawesome/fontawesome-pro-solid";
+import {
+  faFileExcel,
+  faPrint,
+  faCircle
+} from "@fortawesome/fontawesome-pro-solid";
 import Terms from "./Terms";
 import ReactTable from "react-table";
 import Value from "../../elements/Value";
 import { withRouter } from "react-router-dom";
+import classNames from "classnames";
 
 class SearchResults extends React.Component {
   render() {
@@ -145,11 +150,14 @@ class SearchResults extends React.Component {
                     id: "etat",
                     minWidth: 80,
                     accessor: e =>
-                      Value({
-                        value:
-                          e.etablissement.etat_etablissement &&
-                          e.etablissement.etat_etablissement.label,
-                        empty: "-"
+                      e.etablissement.etat_etablissement &&
+                      FontAwesomeIcon({
+                        className: classNames(
+                          e.etablissement.etat_etablissement == "A"
+                            ? "icon--success"
+                            : "icon--danger"
+                        ),
+                        icon: faCircle
                       })
                   },
                   {
@@ -183,13 +191,6 @@ class SearchResults extends React.Component {
                           ),
                         empty: "-"
                       })
-                  },
-                  {
-                    Header: "Activité",
-                    id: "activite",
-                    minWidth: 300,
-                    accessor: e =>
-                      Value({ value: e.etablissement.activite, empty: "-" })
                   },
                   {
                     Header: "Cat. Etablissement",

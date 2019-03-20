b284d8dff54441dac8befdeb2d6045404f2bfd4b
Ozee31 <flavien@commit42.fr>
2019-03-20 14:59:14 +0100

    [server] getUcEff

--- src/server/src/frentreprise/datasources/PG/Etablissements/getUcEff.js
+++ src/server/src/frentreprise/datasources/PG/Etablissements/getUcEff.js
@@ -4,6 +4,41 @@ export default async (SIRET, UcEff) => {
       return null;
     }
 
-    return {};
+    return {
+      unite_controle_competente: getUniteControleCompetente(
+        ucEff.cod_section,
+        ucEff.nme_ddtefp3
+      ),
+      dernier_effectif_physique: ucEff.dereffphy,
+      date_dernier_effectif_physique: getDateDernierEffectifPhysique(
+        ucEff.date_effphy_et
+      ),
+      source_dernier_effectif_physique: null // integer in db
+    };
   });
+};
+
+const getDateDernierEffectifPhysique = date => {
+  try {
+    const [day, month, year] = date.split("/");
+
+    if (!day || !month || !year) {
+      throw new Error("Invalid date");
+    }
+
+    return day && month && year ? `20${year}-${month}-${day}` : null;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
+
+const getUniteControleCompetente = (section, departement) => {
+  let ucc = section;
+
+  if (departement) {
+    ucc += ` (${departement})`;
+  }
+
+  return ucc;
 };

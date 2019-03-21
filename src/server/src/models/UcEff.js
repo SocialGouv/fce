b34ae5de04b78724672408a258e86fb2737d8158
Ozee31 <flavien@commit42.fr>
2019-03-21 16:07:19 +0100

    [server] fix getBySIRET for Idcc & UcEff : not int

--- src/server/src/models/UcEff.js
+++ src/server/src/models/UcEff.js
@@ -3,9 +3,7 @@ import Model from "./Model";
 export default class UcEff extends Model {
   getBySIRET(siret) {
     return this.db
-      .query("SELECT * FROM etablissements_uc_eff WHERE siret = $1",
-        [+siret]
-      )
+      .query("SELECT * FROM etablissements_uc_eff WHERE siret = $1", [siret])
       .then(res => {
         return res.rows && res.rows.length ? res.rows[0] : null;
       })

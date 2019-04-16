ec22836f0457d9d772ff0b158382b0b9316b8f64
valentinEutrope <valentin.eutrope@free.fr>
2019-04-16 17:58:51 +0200

    feat(establishment header): change dashboard look and add conditionnals display

--- src/client/src/components/Enterprise/Sections/Dashboard.js
+++ src/client/src/components/Enterprise/Sections/Dashboard.js
@@ -10,59 +10,83 @@ class Dashboard extends React.Component {
   render() {
     const { establishment } = this.props;
 
+    const isInteractions =
+      establishment.totalInteractions &&
+      establishment.totalInteractions.total > 0;
+
+    const activity = {
+      partialActivity:
+        establishment.activite_partielle_24_derniers_mois.length > 0,
+      pseActivity: establishment.pse_en_projet_ou_en_cours.length > 0
+    };
+
+    console.log(establishment);
+
     return (
       <section id="dashboard" className="enterprise-section dashboard">
         <div className="dashboard-mask" />
         <div className="dashboard-item dashboard-people">
           <div className="dashboard-item--content">
+            <span className="dashboard-item--desc">Effectif</span>
             <span className="dashboard-item--value">
               <Value
                 value={establishment.dernier_effectif_physique}
-                empty="-"
+                empty="0"
                 nonEmptyValues={[0, "0"]}
               />
             </span>
-            <span className="dashboard-item--desc">Effectif</span>
           </div>
         </div>
         <div className="dashboard-item dashboard-interactions">
           <div className="dashboard-item--content">
-            <span className="dashboard-item--value">
-              <Value
-                value={
-                  establishment.totalInteractions &&
-                  establishment.totalInteractions.total
-                }
-                empty=" "
-                nonEmptyValues={[0, "0"]}
-              />
-            </span>
             <span className="dashboard-item--desc">
-              visite(s) et contrôle(s)
+              {isInteractions ? "Visite et contrôle" : "Pas de visite connue"}
             </span>
-          </div>
-        </div>
-        <div className="dashboard-item dashboard-alerts">
-          <div className="dashboard-item--content">
             <span className="dashboard-item--value">
-              <FontAwesomeIcon
-                className="dashboard-icon dashboard-icon"
-                icon={faExclamationTriangle}
-              />
-            </span>
-            <span className="dashboard-item--desc">
-              Signaux faibles à vérifier
+              {isInteractions && (
+                <Value
+                  value={
+                    establishment.totalInteractions &&
+                    establishment.totalInteractions.total
+                  }
+                  empty=" "
+                  nonEmptyValues={[0, "0"]}
+                />
+              )}
             </span>
           </div>
         </div>
-        <div className="dashboard-item dashboard-help">
-          <div className="dashboard-item--content">
-            <span className="dashboard-item--value">
-              <FontAwesomeIcon className="dashboard-icon" icon={faLifeRing} />
-            </span>
-            <span className="dashboard-item--desc">Aides</span>
+        {activity && (activity.pseActivity || activity.partialActivity) && (
+          <div className="dashboard-indicator dashboard-alerts">
+            <div className="dashboard-item--content">
+              <span className="dashboard-item--value">
+                <FontAwesomeIcon
+                  className="dashboard-icon dashboard-icon"
+                  icon={faExclamationTriangle}
+                />
+              </span>
+              <span className="dashboard-item--desc">
+                {activity.partialActivity && "Activité partielle"}
+                <br />
+                {activity.pseActivity && "PSE"}
+              </span>
+            </div>
           </div>
-        </div>
+        )}
+        {activity.partialActivity && (
+          <div className="dashboard-indicator dashboard-help">
+            <div className="dashboard-item--content">
+              <span className="dashboard-item--value">
+                <FontAwesomeIcon
+                  className="dashboard-icon"
+                  color="red"
+                  icon={faLifeRing}
+                />
+              </span>
+              <span className="dashboard-item--desc">Aides</span>
+            </div>
+          </div>
+        )}
       </section>
     );
   }

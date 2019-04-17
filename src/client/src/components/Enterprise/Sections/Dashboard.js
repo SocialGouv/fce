ed75ff94c8d6553b111dbc3dbe8e027617e09791
Mathieu HAAGE <mathieucommit42.fr>
2019-04-17 14:52:54 +0200

    fix(client:dashboard): Display size range string instead of code
    Closes https://trello.com/c/Vi9tYyXB/123-bug-affichage-du-libell%C3%A9-de-la-tranche-effectif-au-lieu-du-code

--- src/client/src/components/Enterprise/Sections/Dashboard.js
+++ src/client/src/components/Enterprise/Sections/Dashboard.js
@@ -23,6 +23,25 @@ class Dashboard extends React.Component {
         establishment.pse_en_projet_ou_en_cours.length > 0
     };
 
+    const sizeRanges = {
+      NN: "Unités non employeuses",
+      "0 salarié": "0 salarié (pas d'effectif au 31/12 )",
+      "01": "1 ou 2 salariés",
+      "02": "3 à 5 salariés",
+      "03": "6 à 9 salariés",
+      "11": "10 à 19 salariés",
+      "12": "20 à 49 salariés",
+      "21": "50 à 99 salariés",
+      "22": "100 à 199 salariés",
+      "31": "200 à 249 salariés",
+      "32": "250 à 499 salariés",
+      "41": "500 à 999 salariés",
+      "42": "1 000 à 1 999 salariés",
+      "51": "2 000 à 4 999 salariés",
+      "52": "5 000 à 9 999 salariés",
+      "53": "10 000 salariés et plus"
+    };
+
     return (
       <section id="dashboard" className="enterprise-section dashboard">
         <div className="dashboard-mask" />
@@ -31,8 +50,8 @@ class Dashboard extends React.Component {
             <span className="dashboard-item--desc">Effectif</span>
             <span className="dashboard-item--value">
               <Value
-                value={establishment.dernier_effectif_physique}
-                empty="0"
+                value={sizeRanges[establishment.tranche_effectif_insee]}
+                empty="-"
                 nonEmptyValues={[0, "0"]}
               />
             </span>

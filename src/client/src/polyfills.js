9dfc3868a50b4dc456dedef959eb025a51465c6c
Ozee31 <flavien@commit42.fr>
2019-04-19 11:12:12 +0200

    build(client:babel): add corejs stable

--- src/client/src/polyfills.js
+++ src/client/src/polyfills.js
@@ -1,4 +1,2 @@
 import "react-app-polyfill/ie11";
-import "core-js/features/array/find";
-import "core-js/features/array/includes";
-import "core-js/features/number/is-nan";
+import "core-js/stable";

d9c698ef1db24a265b25778179c38d36917e27b4
Ozee31 <flavien@commit42.fr>
2019-05-16 14:15:55 +0200

    feat(server:login): send email with magic link
    https://trello.com/c/7k1uhNns/129-etq-utilisateur-je-b%C3%A9n%C3%A9ficie-dune-autorisation-dacc%C3%A8s-personnalis%C3%A9e-%C3%A0-fce-magiclink

--- src/server/src/api/login.js
+++ src/server/src/api/login.js
@@ -1,20 +1,52 @@
-// import MagicLink from "../lib/MagicLink";
 import express from "express";
 import config from "config";
 import MagicKey from "magic-key";
+import Mail from "../utils/mail";
+import sendMagicLinkTpl from "../templates/email/sendMagicLink";
 
 const router = express.Router();
 const magicKey = new MagicKey(config.get("magicKey"));
 
-router.post("/sendMagicLink", function(req, res) {
+router.post("/sendMagicLink", async (req, res) => {
   const { email, clientVerificationKey } = req.body;
-  const key = magicKey.generateKey(email, clientVerificationKey);
+  try {
+    const key = magicKey.generateKey(email, clientVerificationKey);
 
-  // todo: send email
+    if (!key) {
+      throw new Error("Connexion refusée");
+    }
 
-  res.send({
-    success: !!key
-  });
+    const mail = new Mail();
+
+    try {
+      await mail.send(
+        email,
+        "Lien de connexion à FCE",
+        sendMagicLinkTpl({
+          link: `${config.client.baseUrl}${config.client.magicLink.replace(
+            "{key}",
+            key
+          )}`
+        }),
+        { bcc: config.magicLink.bcc }
+      );
+    } catch (e) {
+      console.error("Send magic link email failed", e);
+      throw new Error("L'envoi de l'email a échoué");
+    }
+
+    console.log(`Send magic link to ${email}`);
+
+    return res.send({
+      success: true
+    });
+  } catch (e) {
+    console.error(`Connot send magic link to ${email}`, e, e.message);
+    return res.send({
+      success: false,
+      message: e.message
+    });
+  }
 });
 
 router.post("/checkMagicLink", function(req, res) {

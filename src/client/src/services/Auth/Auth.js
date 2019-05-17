f8eb0c08856ad3da8b37c81a10e65e9ce3495e47
Ozee31 <flavien@commit42.fr>
2019-05-17 11:13:08 +0200

    feat(client:login): generate clientValidationKey + store token

--- src/client/src/services/Auth/Auth.js
+++ src/client/src/services/Auth/Auth.js
@@ -1,23 +1,55 @@
 import { Local } from "OhMyCache";
 import Http from "../Http";
-import Config from "../Config";
+import _get from "lodash.get";
 
-const AUTH_KEY = "fce_auth";
+const AUTH_KEY = "fce_token";
+const AUTH_CLIENT_VERIFICATION_KEY = "fce_client";
 
 export default class Auth {
   static sendMagicLink(email) {
+    const clientVerificationKey = this.generateClientVerificationKey();
+    Local.set(AUTH_CLIENT_VERIFICATION_KEY, clientVerificationKey);
     return Http.post("/sendMagicLink", {
-      email
+      email,
+      clientVerificationKey
     });
   }
 
   static loginWithMagicLink(key) {
+    const clientVerificationKey = Local.get(AUTH_CLIENT_VERIFICATION_KEY);
     return Http.post("/login", {
-      key
+      key,
+      clientVerificationKey
+    }).then(response => {
+      const token = _get(response, "data.token");
+      if (token) {
+        Local.set(AUTH_KEY, token);
+      }
+      return response;
     });
   }
 
   static isLogged() {
     return !!Local.get(AUTH_KEY);
+  }
+
+  static getToken() {
+    return Local.get(AUTH_KEY);
+  }
+
+  /**
+   * @link https://andywalpole.me/blog/140739/using-javascript-create-guid-from-users-browser-information
+   */
+  static generateClientVerificationKey() {
+    const nav = window.navigator;
+    const screen = window.screen;
+    let guid = nav.mimeTypes.length;
+    guid += nav.userAgent.replace(/\D+/g, "");
+    guid += nav.plugins.length;
+    guid += screen.height || "";
+    guid += screen.width || "";
+    guid += screen.pixelDepth || "";
+
+    return guid;
   }
 }

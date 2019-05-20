18ac77e4c0c327dee2d8efef7f4b26979c239dd8
valentinEutrope <valentin.eutrope@free.fr>
2019-05-20 18:12:14 +0200

    feat(Establishment view): restruct poles datas
    Closes https://trello.com/c/HwteFsyD/147-etq-utilisateur-je-peux-voir-un-tableau-d%C3%A9taill%C3%A9-sur-les-derni%C3%A8res-visites-de-chaque-pole-servicess

--- src/client/src/components/Enterprise/Sections/Direccte/EstablishmentView.js
+++ src/client/src/components/Enterprise/Sections/Direccte/EstablishmentView.js
@@ -1,13 +1,9 @@
 import React from "react";
 import Value from "../../../../elements/Value";
 import Data from "./../SharedComponents/Data";
+import { getLastInteraction } from "../../../../helpers/Interactions";
 
 class EstablishmentView extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
   toggleElement = id => {
     this.setState({
       [id]: !this.state[id]
@@ -16,6 +12,21 @@ class EstablishmentView extends React.Component {
 
   render() {
     const { establishment } = this.props;
+
+    const lastInteractions = {
+      pole_T: getLastInteraction(establishment.interactions_T, "T"),
+      pole_C: getLastInteraction(establishment.interactions_C, "C"),
+      pole_3E_SEER: getLastInteraction(
+        establishment.interactions_3E,
+        "3E-SEER"
+      ),
+      pole_3E_SRC: getLastInteraction(establishment.interactions_3E, "3E-SRC")
+    };
+
+    const { pole_T, pole_C, pole_3E_SEER, pole_3E_SRC } = lastInteractions;
+
+    console.log("last interaction", lastInteractions);
+    console.log(establishment);
 
     return (
       <section id="direccte" className="enterprise-section">
@@ -37,46 +48,152 @@ class EstablishmentView extends React.Component {
         />
 
         {establishment.interactions && establishment.interactions.length ? (
-          <table className="table is-hoverable direccte-interactions">
+          <table className="table is-bordered is-hoverable direccte-interactions">
             <thead>
               <tr>
-                <th>Date</th>
                 <th>Pôle</th>
+                <th>Date</th>
                 <th>Objet</th>
                 <th>Unité</th>
                 <th>Agent</th>
                 <th>Type</th>
-                <th>Notes</th>
+                <th>Note</th>
                 <th>Suite</th>
               </tr>
             </thead>
             <tbody>
-              {establishment.interactions.map((dirvis, index) => {
-                return (
-                  <tr key={index}>
-                    <td>
-                      <Value value={dirvis.date} empty="-" />
-                    </td>
-                    <td>
-                      <Value value={dirvis.pole} empty="-" />
-                    </td>
-                    <td> ND </td>
-                    <td>
-                      <Value value={dirvis.unite} empty="-" />
-                    </td>
-                    <td>
-                      <Value value={dirvis.agent} empty="-" />{" "}
-                    </td>
-                    <td>
-                      <Value value={dirvis.type} empty="-" />
-                    </td>
-                    <td>
-                      <Value value={dirvis.note} empty="-" />
-                    </td>
-                    <td> ND </td>
-                  </tr>
-                );
-              })}
+              {pole_T === undefined ? (
+                <tr>
+                  <td>T</td>
+                  <td>Pas de date connue</td>
+                </tr>
+              ) : (
+                <tr>
+                  <td>
+                    <Value value={pole_T.pole} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_T.date} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_T.objet} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_T.unite} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_T.agent} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_T.type} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_T.note} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_T.suite} empty="ND" />
+                  </td>
+                </tr>
+              )}
+              {pole_C === undefined ? (
+                <tr>
+                  <td>C</td>
+                  <td>Données bientôt disponible</td>
+                </tr>
+              ) : (
+                <tr>
+                  <td>
+                    <Value value={pole_C.pole} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_C.date} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_C.objet} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_C.unite} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_C.agent} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_C.type} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_C.note} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_C.suite} empty="ND" />
+                  </td>
+                </tr>
+              )}
+              {pole_3E_SEER === undefined ? (
+                <tr>
+                  <td>3E - SEER</td>
+                  <td>Pas de date connue</td>
+                </tr>
+              ) : (
+                <tr>
+                  <td>
+                    <Value value={pole_3E_SEER.pole} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_3E_SEER.date} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_3E_SEER.objet} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_3E_SEER.unite} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_3E_SEER.agent} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_3E_SEER.type} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_3E_SEER.note} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_3E_SEER.suite} empty="ND" />
+                  </td>
+                </tr>
+              )}
+              {pole_3E_SRC === undefined ? (
+                <tr>
+                  <td>3E - SRC</td>
+                  <td>Données non disponible</td>
+                </tr>
+              ) : (
+                <tr>
+                  <td>
+                    <Value value={pole_3E_SRC.pole} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_3E_SRC.date} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_3E_SRC.objet} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_3E_SRC.unite} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_3E_SRC.agent} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_3E_SRC.type} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_3E_SRC.note} empty="ND" />
+                  </td>
+                  <td>
+                    <Value value={pole_3E_SRC.suite} empty="ND" />
+                  </td>
+                </tr>
+              )}
             </tbody>
           </table>
         ) : null}

c5031dadc3ddfb9e2e8cce0c4e145340ec9db90b
Ozee31 <flavien@commit42.fr>
2019-06-03 21:41:00 +0200

    fix(server:email): correction of a grammatical error

--- src/server/src/templates/email/sendMagicLink.js
+++ src/server/src/templates/email/sendMagicLink.js
@@ -4,7 +4,7 @@ export default ({ link }) =>
   layout(`
 Bonjour,<br/><br/>
 
-Vous avez fait une demande de connexion à l’application FCE, pour vous connecter veuillez cliquez sur le lien suivant : <br/><br/>
+Vous avez fait une demande de connexion à l’application FCE, pour vous connecter veuillez cliquer sur le lien suivant : <br/><br/>
 
 <a href="${link}">${link}</a><br/><br/>
 

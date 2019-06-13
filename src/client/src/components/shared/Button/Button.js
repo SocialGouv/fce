530cb437b496033c89ca0c45f74a31057d30cf34
valentinEutrope <valentin.eutrope@free.fr>
2019-06-13 10:00:26 +0200

    refactor(Client): change proptypes and use classnames

--- src/client/src/components/shared/Button/Button.js
+++ src/client/src/components/shared/Button/Button.js
@@ -1,11 +1,13 @@
 import React from "react";
 import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import ClassNames from "classnames";
 import PropTypes from "prop-types";
 
 const Button = ({ value, icon, buttonClasses, callback }) => {
   return (
     <button
-      className={`button has-text-weight-semibold ${buttonClasses}`}
+      className={`button has-text-weight-semibold ${buttonClasses &&
+        ClassNames(buttonClasses)}`}
       onClick={callback}
     >
       {icon && (
@@ -19,16 +21,15 @@ const Button = ({ value, icon, buttonClasses, callback }) => {
 };
 
 Button.defaultProps = {
-  value: "Click me",
-  icon: false,
+  value: "Valider",
   buttonClasses: "button"
 };
 
 Button.propTypes = {
-  value: PropTypes.string.isRequired,
-  icon: PropTypes.object.isRequired,
-  buttonClasses: PropTypes.string.isRequired,
-  callback: PropTypes.func.isRequired
+  value: PropTypes.string,
+  icon: PropTypes.oneOfType([null, PropTypes.object]),
+  buttonClasses: PropTypes.array,
+  callback: PropTypes.func
 };
 
 export default Button;

3773db9b91daf888c812e9585618a5ac4926ec3d
Mathieu HAAGE <mathieucommit42.fr>
2019-06-16 01:47:18 +0200

    refactor(client:login): Remove many bulma classes
    Closes https://trello.com/c/KI85nmx2/155-revoir-la-page-de-connexion

--- src/client/src/components/Login/steps/LoginHome.js
+++ src/client/src/components/Login/steps/LoginHome.js
@@ -3,14 +3,14 @@ import Button from "../../shared/Button";
 
 const LoginHome = ({ setStep }) => (
   <div className="login__container container">
-    <h1 className="login__title is-size-4-touch is-size-3-desktop has-text-weight-bold has-mb-2">
+    <h1 className="login__title">
       Retrouvez les informations légales et administratives des entreprises
     </h1>
-    <p className="login__subtitle is-size-5-touch is-size-4-desktop has-mb-2">
+    <p className="login__subtitle">
       L'état civil, l'activité et les données de l'administration dans une seule
       fiche destinée aux agents publics
     </p>
-    <p className="has-mb-1">
+    <p className="login__info has-mb-1">
       Ce service est destiné aux agents de service public.
       <br />
       Pour y accéder veuillez vous identifier.

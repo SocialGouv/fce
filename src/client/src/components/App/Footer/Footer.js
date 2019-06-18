d2045b7a0e92a7e2d6ca85a8a270c9e16acb7a7b
Mathieu HAAGE <mathieucommit42.fr>
2019-06-18 14:18:36 +0200

    fix(client): Fix PR
    Closes https://github.com/commit42/fce/pull/155

--- src/client/src/components/App/Footer/Footer.js
+++ src/client/src/components/App/Footer/Footer.js
@@ -1,34 +1,32 @@
 import React, { Component } from "react";
+
+import Config from "../../../services/Config";
 import mariane from "../../../assets/img/logo_gouv.png";
 
-class Footer extends Component {
-  render() {
-    return (
-      <footer className="footer">
-        <div className="container">
-          <div className="footer__gouv">
-            <div className="footer__gouv-text">
-              Un service fourni par l'incubateur des ministères sociaux
-            </div>
-            <img
-              src={mariane}
-              alt="Logo des institutions du gouvernement français"
-              className="footer__gouv-logo"
-            />
-          </div>
-
-          <div className="footer__links">
-            <a
-              className="footer__link"
-              href="mailto:chloe.mandelblat@direccte.gouv.fr"
-            >
-              Contact
-            </a>
-          </div>
+const Footer = () => (
+  <footer className="footer">
+    <div className="container">
+      <div className="footer__gouv">
+        <div className="footer__gouv-text">
+          Un service fourni par l'incubateur des ministères sociaux
         </div>
-      </footer>
-    );
-  }
-}
+        <img
+          src={mariane}
+          alt="Logo des institutions du gouvernement français"
+          className="footer__gouv-logo"
+        />
+      </div>
+
+      <div className="footer__links">
+        <a
+          className="footer__link"
+          href={`mailto:${Config.get("contact.mailto")}`}
+        >
+          Contact
+        </a>
+      </div>
+    </div>
+  </footer>
+);
 
 export default Footer;

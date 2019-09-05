976f5fbf2dd15ff04033531e6a0fb3c208695d56
Ozee31 <flavien@commit42.fr>
2019-09-05 13:04:41 +0200

    feat(server:Accords): db return multi rows for Accords, return total by accord type with lastDate
    https://trello.com/c/Zyjm1jIB/199-mise-%C3%A0-jour-des-indicateurs-li%C3%A9s-aux-accords-dentreprise-sans-restriction-sur-les-ann%C3%A9es-sur-la-fiche-%C3%A9tablisement

--- src/server/src/frentreprise/datasources/PG/Etablissements/getAccords.js
+++ src/server/src/frentreprise/datasources/PG/Etablissements/getAccords.js
@@ -1,9 +1,51 @@
 export default async (SIRET, Accords) => {
   return Accords.getBySIRET(SIRET).then(accords => {
-    if (!accords) {
-      return null;
+    if (!accords || !accords.length) {
+      return {};
     }
 
-    return { accords };
+    const totalAccords = accords.reduce(
+      (acc, accord) => {
+        Object.entries(accord).forEach(([key, value]) => {
+          if (
+            acc.hasOwnProperty(key) &&
+            typeof value === "number" &&
+            value > 0
+          ) {
+            acc[key].count += value;
+
+            if (!acc[key].lastDate || acc[key].lastDate < accord.dt_sign) {
+              acc[key].lastDate = accord.dt_sign;
+            }
+          }
+        });
+        return acc;
+      },
+      {
+        autres: { count: 0, lastDate: null },
+        classifications: { count: 0, lastDate: null },
+        conditions_travail: { count: 0, lastDate: null },
+        droit_syndical: { count: 0, lastDate: null },
+        egalite_pro: { count: 0, lastDate: null },
+        emploi: { count: 0, lastDate: null },
+        epargne: { count: 0, lastDate: null },
+        formation: { count: 0, lastDate: null },
+        nouvelles_technologies: { count: 0, lastDate: null },
+        protection_sociale: { count: 0, lastDate: null },
+        remuneration: { count: 0, lastDate: null },
+        temps_travail: { count: 0, lastDate: null }
+      }
+    );
+
+    totalAccords.total = {
+      count: accords.length,
+      lastDate: Object.values(accords).reduce(
+        (lastDate, { dt_sign }) =>
+          !lastDate || lastDate < dt_sign ? dt_sign : lastDate,
+        null
+      )
+    };
+
+    return { accords: totalAccords };
   });
 };

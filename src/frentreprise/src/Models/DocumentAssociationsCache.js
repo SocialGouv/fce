b100d6d37cb47e8e31ebf8dac679b345908fe435
ozee31 <flavien.beninca@gmail.com>
2019-10-01 17:23:46 +0200

    feat(frentreprise:document_association): add db cache
    because api entrypoint it's too slow
    
    https://trello.com/c/E24GQmkI/194-diagnostiquer-le-souci-de-lenteur-sur-louverture-de-la-fiche-etablissement-appel-api-entreprise

---
+++ src/frentreprise/src/Models/DocumentAssociationsCache.js
@@ -0,0 +1,17 @@
+import Model from "./Model";
+
+export default class DocumentAssociationsCache extends Model {
+  getBySiret(siret) {
+    return this.db
+      .query("SELECT * FROM document_associations_cache WHERE siret = $1", [
+        siret
+      ])
+      .then(res => {
+        return res && res.rows && res.rows.length ? res.rows[0] : null;
+      })
+      .catch(e => {
+        console.error("DocumentAssociationsCache::getBySiret", e);
+        return null;
+      });
+  }
+}

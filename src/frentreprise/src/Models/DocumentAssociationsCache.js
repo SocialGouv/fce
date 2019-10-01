b67ebfcb592bd3849cfceb388a971f8a32a30fff
ozee31 <flavien.beninca@gmail.com>
2019-10-01 17:58:11 +0200

    feat(frentreprise:DocumentAssociationsCache): update sql query to add sort by date
    DocumentAssociationsCache

--- src/frentreprise/src/Models/DocumentAssociationsCache.js
+++ src/frentreprise/src/Models/DocumentAssociationsCache.js
@@ -3,9 +3,10 @@ import Model from "./Model";
 export default class DocumentAssociationsCache extends Model {
   getBySiret(siret) {
     return this.db
-      .query("SELECT * FROM document_associations_cache WHERE siret = $1", [
-        siret
-      ])
+      .query(
+        "SELECT * FROM document_associations_cache WHERE siret = $1 ORDER BY createdAt DESC",
+        [siret]
+      )
       .then(res => {
         return res && res.rows && res.rows.length ? res.rows[0] : null;
       })

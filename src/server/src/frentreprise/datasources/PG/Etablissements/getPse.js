e8a443bfefc58c017495457fc998cdf84dc96292
valentinEutrope <valentin.eutrope@free.fr>
2019-10-02 17:40:10 +0200

    feat(server): can receive multiple lines

--- src/server/src/frentreprise/datasources/PG/Etablissements/getPse.js
+++ src/server/src/frentreprise/datasources/PG/Etablissements/getPse.js
@@ -1,14 +1,36 @@
 export default async (SIRET, rows) => {
-  return rows.getBySIRET(SIRET).then(pse => {
-    if (!pse) {
+  return rows.getBySIRET(SIRET).then(pseRows => {
+    if (!pseRows || pseRows.length === 0) {
       return {};
     }
 
+    const pse = pseRows.map(pseRow => {
+      return {
+        numero_de_dossier: pseRow.numero_de_dossier
+          ? pseRow.numero_de_dossier.trim()
+          : null,
+        type_de_dossier: pseRow.type_de_dossier
+          ? pseRow.type_de_dossier.trim()
+          : null,
+        date_enregistrement: pseRow.date_d_enregistrement,
+        etat_du_dossier: pseRow.etat_du_dossier
+          ? pseRow.etat_du_dossier.trim()
+          : null,
+        accord_signe: pseRow.accord_signe,
+        date_de_jugement: pseRow.date_de_jugement,
+        situation_juridique: pseRow.situation_juridique
+          ? pseRow.situation_juridique.trim()
+          : null,
+        siret: pseRow.siret,
+        rupture_contrat_debut:
+          pseRow.nombre_de_ruptures_de_contrats_en_debut_de_procedure,
+        rupture_contrat_fin:
+          pseRow.nombre_de_ruptures_de_contrats_en_fin_de_procedure
+      };
+    });
+
     return {
-      pse: {
-        rupture_contrat_debut: pse.rupturecontratdebut,
-        rupture_contrat_fin: pse.rupturecontratfin
-      }
+      pse: pse
     };
   });
 };
--- src/server/src/models/Pse.js
+++ src/server/src/models/Pse.js
@@ -5,7 +5,7 @@ export default class Pse extends Model {
     return this.db
       .query("SELECT * FROM etablissements_pse WHERE siret = $1", [siret])
       .then(res => {
-        return res.rows && res.rows.length ? res.rows[0] : null;
+        return res.rows;
       })
       .catch(e => {
         console.error("Pse::getBySIRET", e);

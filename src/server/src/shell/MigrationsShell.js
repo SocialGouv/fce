bd549bd034474de2ed39f38b329437d8649019ab
ozee31 <flavien.beninca@gmail.com>
2019-10-02 15:46:47 +0200

    fix(server:migrationsShell): use process.env instead of config
    parse error in preprod env

--- src/server/src/shell/MigrationsShell.js
+++ src/server/src/shell/MigrationsShell.js
@@ -1,12 +1,18 @@
-const Shell = require("./Shell");
-
-const config = require("config");
+require("dotenv").config();
 const MigrationRunner = require("node-pg-migrate/lib/runner");
+const process = require("process");
+
+const Shell = require("./Shell");
 
 class MigrationsShell extends Shell {
   async execute() {
     const options = {
-      databaseUrl: config.get("db"),
+      databaseUrl: {
+        host: process.env.PG_HOST,
+        user: process.env.PG_USER,
+        password: process.env.PG_PASSWORD,
+        database: process.env.PG_DB
+      },
       dir: "migrations",
       migrationsTable: "pgmigrations",
       direction: "up"

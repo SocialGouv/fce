dd5b8ae37cef1e687dd991231ec8e33365c41c28
Mathieu HAAGE <mathieucommit42.fr>
2019-10-02 16:22:58 +0200

    refactor(server:shell): Improve code
    Closes https://trello.com/c/JawrmWLo/188-etq-utilisateur-je-consulte-les-donn%C3%A9es-sur-les-mut-eco-pse-et-rcc-1-2-transpose-des-variables-etab-du-fichier-de-colonnes-en-li

--- src/server/src/shell/ConvertPseCsvShell.js
+++ src/server/src/shell/ConvertPseCsvShell.js
@@ -24,26 +24,25 @@ class ConvertPseCsvShell extends Shell {
     csv
       .parseStream(process.stdin, { headers: true, delimiter: "," })
       .transform(data => {
-        const establishmentLabels = Object.entries(data)
+        Object.entries(data)
           .filter(([key, value]) => key.includes("SIRET_Etab") && value !== "")
-          .map(establishments => establishments[0].slice(6));
-
-        establishmentLabels.forEach(establishment => {
-          stream.write({
-            numero_de_dossier: data.Numero_de_dossier,
-            type_de_dossier: data.Type_de_dossier,
-            etat_du_dossier: data.Etat_du_dossier,
-            accord_signe: data.Accord_Signe,
-            date_de_jugement: data.Date_de_jugement,
-            date_d_enregistrement: data.date_enregistrement,
-            situation_juridique: data.situation_juridique,
-            siret: data[`SIRET_${establishment}`],
-            nombre_de_ruptures_de_contrats_en_debut_de_procedure:
-              data[`Nb_Rupture_debut_${establishment}`],
-            nombre_de_ruptures_de_contrats_en_fin_de_procedure:
-              data[`Nb_Rupture_fin__${establishment}`]
+          .map(establishments => establishments[0].slice(6))
+          .forEach(establishment => {
+            stream.write({
+              numero_de_dossier: data.Numero_de_dossier,
+              type_de_dossier: data.Type_de_dossier,
+              etat_du_dossier: data.Etat_du_dossier,
+              accord_signe: data.Accord_Signe,
+              date_de_jugement: data.Date_de_jugement,
+              date_d_enregistrement: data.date_enregistrement,
+              situation_juridique: data.situation_juridique,
+              siret: data[`SIRET_${establishment}`],
+              nombre_de_ruptures_de_contrats_en_debut_de_procedure:
+                data[`Nb_Rupture_debut_${establishment}`],
+              nombre_de_ruptures_de_contrats_en_fin_de_procedure:
+                data[`Nb_Rupture_fin__${establishment}`]
+            });
           });
-        });
       });
   }
 }

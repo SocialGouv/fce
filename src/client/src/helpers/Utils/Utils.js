b9e3931676bf87feda695f5784ec6ecac260608f
valentinEutrope <valentin.eutrope@free.fr>
2019-10-11 12:17:27 +0200

    fix(client: helpers): fusion utils folders

--- src/client/src/helpers/Utils/Utils.js
+++
@@ -1,7 +0,0 @@
-export const CountValueInArray = (array, key) =>
-  array.reduce((acc, currentValue) => {
-    return acc + currentValue[key];
-  }, 0);
-
-export const hasInclude = (str, arrayOfStrings) =>
-  arrayOfStrings.some(item => str.includes(item));

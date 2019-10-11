42c219ea2499c43cd5f0745d4c6ad2e010212eec
valentinEutrope <valentin.eutrope@free.fr>
2019-10-11 11:49:59 +0200

    feat(client: helpers): create a helper to verify string contain string

--- src/client/src/helpers/Utils/Utils.js
+++ src/client/src/helpers/Utils/Utils.js
@@ -2,3 +2,6 @@ export const CountValueInArray = (array, key) =>
   array.reduce((acc, currentValue) => {
     return acc + currentValue[key];
   }, 0);
+
+export const hasInclude = (str, arrayOfStrings) =>
+  arrayOfStrings.some(item => str.includes(item));

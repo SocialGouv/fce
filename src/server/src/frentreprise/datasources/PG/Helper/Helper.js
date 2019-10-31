9f8c6ac3f56c7660b2886fe3ae7e0ce3dc1cfcf5
ozee31 <flavien.beninca@gmail.com>
2019-10-31 11:21:57 +0100

    refactor(server:PGHelper): use date-fns to parse and reformate dates
    https://trello.com/c/8XAmtEYO/222-maj-donn%C3%A9es-pole-3e-seer-eos

--- src/server/src/frentreprise/datasources/PG/Helper/Helper.js
+++ src/server/src/frentreprise/datasources/PG/Helper/Helper.js
@@ -1,53 +1,21 @@
+import { parse, isValid, format } from "date-fns";
+
 export const getFormatedDate = date => {
   if (!date) {
     return null;
   }
 
-  const trimedDate = date.trim();
-  const defaultFormatedDate = extractDefaultFormatedDate(trimedDate);
+  date = date.trim();
 
-  if (defaultFormatedDate) {
-    return defaultFormatedDate;
+  const datesFormats = ["dd/MM/yyyy", "ddMMMyyyy"];
+
+  for (const dateFormat of datesFormats) {
+    const parsedDate = parse(date, dateFormat, new Date());
+
+    if (isValid(parsedDate)) {
+      return format(parsedDate, "yyyy-MM-dd");
+    }
   }
 
-  return extractSecondaryFormatedDate(trimedDate);
-};
-
-const extractDefaultFormatedDate = date => {
-  const [day, month, year] = date.split("/");
-
-  if (!day || !month || !year) {
-    return null;
-  }
-
-  return `${year}-${month}-${day}`;
-};
-
-const extractSecondaryFormatedDate = date => {
-  const getMonthByName = monthName =>
-    ({
-      JAN: "01",
-      FEB: "02",
-      MAR: "03",
-      APR: "04",
-      MAY: "05",
-      JUN: "06",
-      JUL: "07",
-      AUG: "08",
-      SEP: "09",
-      OCT: "10",
-      NOV: "11",
-      DEC: "12"
-    }[monthName]);
-
-  const day = date.substring(0, 2);
-  const monthName = date.substring(2, 5);
-  const month = getMonthByName(monthName);
-  const year = date.substring(5);
-
-  if (!day || !month || !year) {
-    return null;
-  }
-
-  return `${year}-${month}-${day}`;
+  return null;
 };

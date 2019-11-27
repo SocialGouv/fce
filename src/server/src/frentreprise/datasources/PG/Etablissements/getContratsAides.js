46c25c525f17a45ace921a14030660a4bd72380f
imhaage <mahaage@gmail.com>
2019-11-27 11:35:15 +0100

    fix(EstablishmentsHelps): Really fix PR this time!

--- src/server/src/frentreprise/datasources/PG/Etablissements/getContratsAides.js
+++ src/server/src/frentreprise/datasources/PG/Etablissements/getContratsAides.js
@@ -8,7 +8,7 @@ export default async (SIRET, ContratsAides) => {
       contrat_aide: !!contratAide.contrat_aide,
       contrat_aide_salaries_n1: contratAide.CA_stock_12_2018,
       contrat_aide_embauches_n1: contratAide.CA_entree_2018,
-      contrat_aide_alternance_n1: contratAide.CA_contrat_2018
+      contrat_aide_alternance_n1: null
     };
   });
 };

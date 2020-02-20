bb160b2bcaa1b911f30336a935d3ed788d065abd
imhaage <mahaage@gmail.com>
2020-02-20 11:23:25 +0100

    feat(client:MagicLink): Force user to copy/paste link in the right browser
    Closes https://trello.com/c/1pLzwUXY/267-rendre-le-lien-des-emails-non-cliquables-et-mettre-en-valeur-la-mention-copier-coller

--- src/server/src/templates/email/sendMagicLink.js
+++ src/server/src/templates/email/sendMagicLink.js
@@ -11,19 +11,13 @@ export default ({ link, browser }) =>
 
     <tr>
       <td style="padding-bottom: 16px;">
-        Vous avez fait une demande de connexion à l’application FCE, pour vous connecter veuillez copier/coller le lien suivant dans votre navigateur ${browser} :
+        Vous avez fait une demande de connexion à l’application FCE, pour vous connecter veuillez <strong>copier/coller le lien suivant dans votre navigateur ${browser}</strong> :
       </td>
     </tr>
 
     <tr>
-      <td width="100%;" style="word-break: break-all; padding-bottom: 16px;">
-        <a style="color: #2980b9" href="${link}">${link}</a>
-      </td>
-    </tr>
-
-    <tr>
-      <td style="padding-bottom: 16px;">
-        <strong>Attention, ce lien n’est valable que pour le navigateur ${browser} avec lequel vous avez fait la demande. Si votre navigateur par défaut est différent veuillez copier coller le lien dans celui-ci.</strong>
+      <td width="100%;" style="font-size: 13px; color: #666; word-break: break-all; padding-bottom: 16px;">
+        ${link}
       </td>
     </tr>
 

54afef51180d3a3a07c4f63b2200ab7791899877
imhaage <mahaage@gmail.com>
2020-03-04 14:25:18 +0100

    feat(server): Create /sources endpoint
    Closes https://trello.com/c/qNK1KsG3/201-etq-utilisateur-je-peux-voir-la-source-et-date-de-mise-%C3%A0-jour-de-chaque-indicateur-fiche-etablissement

--- src/server/src/api/index.js
+++ src/server/src/api/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const searchRouter = require("./search");
 const loginRouter = require("./login");
 const usersFeedbackRouter = require("./usersFeedback");
+const sourcesRouter = require("./sources");
 
 router.use("", searchRouter);
 router.use("", loginRouter);
 router.use("", usersFeedbackRouter);
+router.use("", sourcesRouter);
 
 module.exports = router;

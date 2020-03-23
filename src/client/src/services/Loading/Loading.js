720eabeeffbf2a85ae0066de89dfb3c56665f6ab
valentinEutrope <valentin.eutrope@free.fr>
2020-03-23 17:56:25 +0100

    style(client: HOC): center content

--- src/client/src/services/Loading/Loading.js
+++ src/client/src/services/Loading/Loading.js
@@ -2,14 +2,16 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/pro-solid-svg-icons";
 
-export default function withLoading(BaseComponent) {
+const withLoading = BaseComponent => {
   return function({ isLoaded, ...props }) {
     return isLoaded ? (
       <BaseComponent {...props} />
     ) : (
-      <div className="text-center">
+      <div className="container">
         <FontAwesomeIcon icon={faSpinner} spin /> Chargement en cours...
       </div>
     );
   };
-}
+};
+
+export default withLoading;

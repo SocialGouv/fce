fcf82d9f52de33173c230ac64bb0b7fbd716bc5b
valentinEutrope <valentin.eutrope@free.fr>
2020-03-24 15:54:49 +0100

    fix(client): double import

--- src/client/src/components/DataSheets/Sections/Enterprise/Enterprise.js
+++ src/client/src/components/DataSheets/Sections/Enterprise/Enterprise.js
@@ -1,6 +1,5 @@
 import PropTypes from "prop-types";
 import React from "react";
-import PropTypes from "prop-types";
 import { faPrint } from "@fortawesome/pro-solid-svg-icons";
 import withLoading from "../../../../services/withLoading";
 import Sidebar from "../../Sidebar";

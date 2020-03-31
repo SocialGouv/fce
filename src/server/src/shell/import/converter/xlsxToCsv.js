5609f200d4ff8f9888475d7dc81472194d0f661c
ozee31 <flavien.beninca@gmail.com>
2020-03-31 14:55:55 +0200

    chore(server:import): add eslint-disable-next-line security/detect-non-literal-fs-filename to xlsxTo

--- src/server/src/shell/import/converter/xlsxToCsv.js
+++ src/server/src/shell/import/converter/xlsxToCsv.js
@@ -1,6 +1,8 @@
 const xlsx = require("xlsx");
 
 module.exports = async filename => {
+  // eslint-disable-next-line security/detect-non-literal-fs-filename
   const workBook = xlsx.readFile(filename);
+  // eslint-disable-next-line security/detect-non-literal-fs-filename
   return xlsx.writeFile(workBook, filename, { bookType: "csv" });
 };

378be6fa4990eb8c94fcb81fd6254ff043a9eded
valentinEutrope <valentin.eutrope@free.fr>
2020-04-10 13:55:59 +0200

    feat(client: pages): add userFeedback on footer pages

--- src/client/src/components/About/About.js
+++ src/client/src/components/About/About.js
@@ -1,15 +1,19 @@
 import React, { lazy, Suspense } from "react";
 import { importMDX } from "mdx.macro";
+import UsersFeedback from "../../containers/UsersFeedback";
 
 const Content = lazy(() => importMDX("./About.mdx"));
 
 const About = () => {
   return (
-    <div className="page">
-      <Suspense fallback="">
-        <Content />
-      </Suspense>
-    </div>
+    <>
+      <div className="page">
+        <Suspense fallback="">
+          <Content />
+        </Suspense>
+      </div>
+      <UsersFeedback fullWidth />
+    </>
   );
 };
 
--- src/client/src/components/LegalNotices/LegalNotices.js
+++ src/client/src/components/LegalNotices/LegalNotices.js
@@ -1,15 +1,19 @@
 import React, { lazy, Suspense } from "react";
 import { importMDX } from "mdx.macro";
+import UsersFeedback from "../../containers/UsersFeedback";
 
 const Content = lazy(() => importMDX("./LegalNotices.mdx"));
 
 const LegalNotices = () => {
   return (
-    <div className="page">
-      <Suspense fallback="">
-        <Content />
-      </Suspense>
-    </div>
+    <>
+      <div className="page">
+        <Suspense fallback="">
+          <Content />
+        </Suspense>
+      </div>
+      <UsersFeedback fullWidth />
+    </>
   );
 };
 

2d9c7b766a2e7cce125717b03cec5a4e6d904523
ozee31 <flavien.beninca@gmail.com>
2020-04-22 11:11:30 +0200

    feat(server:getFormatedDate): add outputFormat to helper getFormatedDate

--- src/server/src/frentreprise/datasources/PG/Helper/Helper.js
+++ src/server/src/frentreprise/datasources/PG/Helper/Helper.js
@@ -1,6 +1,6 @@
 import { parse, isValid, format } from "date-fns";
 
-export const getFormatedDate = date => {
+export const getFormatedDate = (date, outputFormat = "yyyy-MM-dd") => {
   if (!date) {
     return null;
   }
@@ -12,14 +12,14 @@ export const getFormatedDate = date => {
     "dd/MM/yyyy",
     "ddMMMyyyy",
     "dd/MM/yy",
-    "M/d/yy"
+    "M/d/yy",
   ];
 
   for (const dateFormat of datesFormats) {
     const parsedDate = parse(date, dateFormat, new Date());
 
     if (isValid(parsedDate)) {
-      return format(parsedDate, "yyyy-MM-dd");
+      return format(parsedDate, outputFormat);
     }
   }
 

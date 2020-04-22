e51519af98b6ca916526da3701a759a902487b5b
ozee31 <flavien.beninca@gmail.com>
2020-04-22 11:52:06 +0200

    refactor(client:EstablishmentHelps): extract subcategories to components
    https://trello.com/c/3dCGqj15/284-back-etq-utilisateur-je-consulte-les-donn%C3%A9es-apprentissage-de-la-sous-section-alternance-fiche-etab-fiche-ent

--- src/client/src/components/DataSheets/Sections/Establishment/Helps/EstablishmentHelps.js
+++ src/client/src/components/DataSheets/Sections/Establishment/Helps/EstablishmentHelps.js
@@ -1,22 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
-import _get from "lodash.get";
-import Value from "../../../../shared/Value";
-import Subcategory from "../../SharedComponents/Subcategory";
-import Data from "../../SharedComponents/Data";
-import Config from "../../../../../services/Config";
-import { getCustomPastYear } from "../../../../../helpers/Date/Date";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMedkit } from "@fortawesome/pro-solid-svg-icons";
 
-const EstablishmentHelps = ({ establishment }) => {
-  const hasAgrements = !!(
-    establishment.agrements_iae &&
-    Object.values(establishment.agrements_iae)
-      .map(agrementData => agrementData.agrement)
-      .includes(true)
-  );
+import Agrements from "./Subcategory/Agrements";
+import ContratsAides from "./Subcategory/ContratsAides";
 
+const EstablishmentHelps = ({ establishment }) => {
   return (
     <section id="helps" className="data-sheet__section">
       <div className="section-header">
@@ -26,101 +16,8 @@ const EstablishmentHelps = ({ establishment }) => {
         <h2 className="title">Aides et agréments</h2>
       </div>
       <div className="section-datas">
-        <Subcategory
-          subtitle="Agréments"
-          datas={[
-            {
-              name: "Agrément(s) Insertion par l’activité économique (IAE)",
-              value: hasAgrements,
-              columnClasses: ["is-7", "is-5"],
-              source: "ASP Extranet IAE2.0"
-            }
-          ]}
-        />
-
-        {hasAgrements && (
-          <table className="table is-hoverable">
-            <thead>
-              <tr>
-                <th />
-                <th>Agrément(s) en {getCustomPastYear(1)}</th>
-                <th>
-                  Nombre de salariés en insertion présents en{" "}
-                  {getCustomPastYear(1)}
-                </th>
-                <th>Nombre d’ETP en année {getCustomPastYear(1)}</th>
-              </tr>
-            </thead>
-
-            <tbody>
-              {Object.entries(Config.get("agrementsIae")).map(
-                ([key, label]) => {
-                  const { etp, agrement, salariesInsertion } = _get(
-                    establishment,
-                    `agrements_iae[${key}]`
-                  );
-
-                  return (
-                    <tr key={key}>
-                      <th>{label}</th>
-                      <td>
-                        <Value value={agrement} />
-                      </td>
-                      <td>
-                        <Value value={salariesInsertion} />
-                      </td>
-                      <td>
-                        <Value value={etp && Math.round(etp)} />
-                      </td>
-                    </tr>
-                  );
-                }
-              )}
-            </tbody>
-          </table>
-        )}
-
-        <Subcategory subtitle="Contrats aidés">
-          <Data
-            name={`Parcours emploi – compétences (PEC) ou Emploi d’avenir en ${getCustomPastYear(
-              1
-            )}`}
-            value={!!establishment.contrat_aide}
-            columnClasses={["is-7", "is-5"]}
-            sourceSi="ASP Extranet CUI"
-          />
-          {establishment.contrat_aide && (
-            <table className="table is-bordered mt-3">
-              <thead>
-                <tr>
-                  <th>
-                    Nombre de salariés présents au 31/12/
-                    {getCustomPastYear(1)}
-                  </th>
-                  <th>
-                    Nombre de salariés embauchés en {getCustomPastYear(1)}
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>
-                    <Value
-                      value={establishment.contrat_aide_salaries_n1}
-                      nonEmptyValue={[0]}
-                    />
-                  </td>
-                  <td>
-                    <Value
-                      value={establishment.contrat_aide_embauches_n1}
-                      nonEmptyValue={[0]}
-                    />
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          )}
-        </Subcategory>
+        <Agrements establishment={establishment} />
+        <ContratsAides establishment={establishment} />
       </div>
     </section>
   );

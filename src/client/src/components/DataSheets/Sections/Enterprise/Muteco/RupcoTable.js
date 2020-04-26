7ef40aad591379e3f6cc1c1480aa6adcfd03d03a
imhaage <mahaage@gmail.com>
2020-04-26 15:22:16 +0200

    refactor(client): Remove console.log()
    Closes https://trello.com/c/4XLWHXCR/276-etx-jepeux-consulter-donnes-mut-eco-maj-pse-et-rcc

--- src/client/src/components/DataSheets/Sections/Enterprise/Muteco/RupcoTable.js
+++ src/client/src/components/DataSheets/Sections/Enterprise/Muteco/RupcoTable.js
@@ -2,38 +2,33 @@ import React from "react";
 import PropTypes from "prop-types";
 import AccordionTable from "./AccordionTable";
 
-const RupcoTable = ({ list, hasTypeColumn = false }) => {
-  console.log({ list });
-  return (
-    <div className="is-overflow-x">
-      <table className="table mt-2 is-max-content">
-        <thead>
-          <tr>
-            {hasTypeColumn && <th>Type</th>}
-            <th>Date d{"'"}enregistrement</th>
-            <th>Numéro de dossier</th>
-            <th>État du dossier</th>
-            <th>
-              Situation juridique de l{"'"}entreprise au moment de la procédure
-            </th>
-            <th>Date du jugement</th>
-            <th>
-              Nombre maximum de ruptures de contrats de travail envisagées
-            </th>
-            <th>Nombre d{"'"}établissements impactés</th>
-          </tr>
-        </thead>
-        {list.map(procedure => (
-          <AccordionTable
-            procedure={procedure}
-            key={`${procedure}`}
-            hasTypeColumn={hasTypeColumn}
-          />
-        ))}
-      </table>
-    </div>
-  );
-};
+const RupcoTable = ({ list, hasTypeColumn = false }) => (
+  <div className="is-overflow-x">
+    <table className="table mt-2 is-max-content">
+      <thead>
+        <tr>
+          {hasTypeColumn && <th>Type</th>}
+          <th>Date d{"'"}enregistrement</th>
+          <th>Numéro de dossier</th>
+          <th>État du dossier</th>
+          <th>
+            Situation juridique de l{"'"}entreprise au moment de la procédure
+          </th>
+          <th>Date du jugement</th>
+          <th>Nombre maximum de ruptures de contrats de travail envisagées</th>
+          <th>Nombre d{"'"}établissements impactés</th>
+        </tr>
+      </thead>
+      {list.map(procedure => (
+        <AccordionTable
+          procedure={procedure}
+          key={`${procedure}`}
+          hasTypeColumn={hasTypeColumn}
+        />
+      ))}
+    </table>
+  </div>
+);
 
 RupcoTable.propTypes = {
   list: PropTypes.array.isRequired,

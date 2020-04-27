8c8f4f723d096f0968d22ccb09618c2df9354df6
ozee31 <flavien.beninca@gmail.com>
2020-04-27 15:51:35 +0200

    feat(frentreprise:apiGouv): remove call without privileges

--- src/frentreprise/src/DataSources/ApiGouv/EntreprisesAPI/index.js
+++ src/frentreprise/src/DataSources/ApiGouv/EntreprisesAPI/index.js
@@ -1,14 +1,10 @@
 import getEntreprise from "./getEntreprise";
-import attestation_dgfip from "./attestation_dgfip";
-import attestation_acoss from "./attestation_acoss";
 import infogreffe_rcs from "./infogreffe_rcs";
 import effectifsMensuelEtp from "./effectifsMensuelEtp";
 import effectifsAnnuelEtp from "./effectifsAnnuelEtp";
 
 export default {
   getEntreprise,
-  attestation_dgfip,
-  attestation_acoss,
   infogreffe_rcs,
   effectifsMensuelEtp,
   effectifsAnnuelEtp,

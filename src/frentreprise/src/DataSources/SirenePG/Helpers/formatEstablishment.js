a656549e2292e50c3225982a85fdbd903a4f9682
ozee31 <flavien.beninca@gmail.com>
2020-06-12 13:26:04 +0200

    feat(frentreprise:pg): load and formate establishment (just SIRENE)
    https://trello.com/c/ZNWDQhKl/327-optimisation-des-requ%C3%AAtes-postgres

---
+++ src/frentreprise/src/DataSources/SirenePG/Helpers/formatEstablishment.js
@@ -0,0 +1,135 @@
+import utils from "../../../Utils/utils";
+import getData from "../../getData";
+
+export default async (etablishment) => {
+  const getAdresseComponent = ({
+    numerovoieetablissement,
+    indicerepetitionetablissement,
+    typevoieetablissement,
+    libellevoieetablissement,
+    complementadresseetablissement,
+    codepostaletablissement,
+    codecommuneetablissement,
+    libellecommuneetablissement,
+  }) => {
+    return {
+      numero_voie: numerovoieetablissement,
+      indice_repetition: indicerepetitionetablissement,
+      type_voie: typevoieetablissement,
+      nom_voie: libellevoieetablissement,
+      complement_adresse: complementadresseetablissement,
+      code_postal: codepostaletablissement,
+      code_insee_localite: codecommuneetablissement,
+      localite: libellecommuneetablissement,
+    };
+  };
+  return getData(etablishment, [
+    "siret",
+    {
+      in: "etatadministratifetablissement",
+      out: "actif",
+      callback: (etat) => etat && etat === "A",
+    },
+    {
+      in: "etatadministratifetablissement",
+      out: "etat_etablissement",
+    },
+    {
+      in: "etatadministratifetablissement",
+      out: "etat_etablissement_libelle",
+      callback: (etat) => {
+        switch (etat) {
+          case "A":
+            return "Actif";
+          case "F":
+            return "Fermé";
+          case "C":
+            return "Cessé";
+          default:
+            return undefined;
+        }
+      },
+    },
+    {
+      in: "datedebut",
+      out: "date_fin",
+    },
+    {
+      in: "datederniertraitementetablissement",
+      out: "date_dernier_traitement_etablissement",
+    },
+    {
+      in: "enseigne1etablissement",
+      out: "enseigne",
+    },
+    {
+      in: "naf.code",
+      out: "naf",
+    },
+    {
+      in: "naf.libelle",
+      out: "libelle_naf",
+    },
+    {
+      in: "etablissementsiege",
+      out: "siege_social",
+      callback: (siege) => siege === "true",
+    },
+    {
+      in: "etablissementsiege",
+      out: "categorie_etablissement",
+      callback: (siege_social) =>
+        utils.isEmpty(siege_social)
+          ? undefined
+          : siege_social === "true"
+          ? "Siège social"
+          : "Établissement secondaire",
+    },
+    {
+      in: "datecreationetablissement",
+      out: "date_creation",
+    },
+    {
+      in: "trancheeffectifsetablissement",
+      out: "tranche_effectif_insee",
+    },
+    {
+      in: "anneeeffectifsetablissement",
+      out: "annee_tranche_effectif_insee",
+    },
+    {
+      in: "complementadresseetablissement",
+      out: "adresse_components",
+      callback: (complementAdresse, etab) => {
+        const adresseComponent = getAdresseComponent(etab);
+        return utils.isEmpty(adresseComponent) ? undefined : adresseComponent;
+      },
+    },
+    {
+      in: "complementadresseetablissement",
+      out: "adresse",
+      callback: (complementAdresse, etab) => {
+        const adresseComponent = getAdresseComponent(etab);
+        return utils.isEmpty(adresseComponent)
+          ? undefined
+          : utils.getCleanAddress(adresseComponent);
+      },
+    },
+    {
+      in: "caractereemployeuretablissement",
+      out: "etablissement_employeur",
+    },
+    {
+      in: "entreprise.denominationunitelegale",
+      out: "nom_commercial",
+    },
+    {
+      in: "entreprise.nomunitelegale",
+      out: "nom",
+    },
+    {
+      in: "entreprise.prenom1unitelegale",
+      out: "prenom",
+    },
+  ]);
+};
--- src/frentreprise/src/DataSources/SirenePG/Siret/getSettlement.js
+++ src/frentreprise/src/DataSources/SirenePG/Siret/getSettlement.js
@@ -1,6 +1,7 @@
 import Etablissement from "../../../Models/Etablissements";
 import helpers from "../Helpers/helpers";
 import models from "../../../Model";
+import formatEstablishment from "../Helpers/formatEstablishment";
 
 const getSettlement = async (SIRET, db) => {
   const includes = [
@@ -22,33 +23,11 @@ const getSettlement = async (SIRET, db) => {
     include: includes,
   });
 
-  console.log(
-    "////////////////etablissement 007",
-    etablissement,
-    etablissement.rupcoEtablissements[0]
-    // { pse: etablissement.pse, rcc: etablissement.rcc, lice: etablissement.lice }
-    // etablissement.naf.libelle,
-    // etablissement.entreprise.datecreationunitelegale
-  );
-
-  console.log("***");
-  console.log(includes);
-
-  return {};
-  // console.log({ db });
-  // const departements = await models.Departement.findAll();
-  // const cjs = await models.CategorieJuridique.findAll();
-  // // const etab = await models.Etablissement.findAll(); // LIMIT !!!!
-  // // console.log({ departements });
-  // console.log({ departement: departements[0] });
-  // console.log({ cj: cjs[0] });
-  // console.log({ etab: etab[0] });
-
   if (!etablissement) {
     return {};
   }
 
-  return await helpers.formatEtab(etablissement);
+  return await formatEstablishment(etablissement);
 };
 
 export default getSettlement;

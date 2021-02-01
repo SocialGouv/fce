22b71eafb04cf54802fc84d59258b71831791a16
Eutrope Valentin <valentin.eutrope@free.fr>
2021-02-01 17:47:00 +0100

    feat(server: api): update api, update fetch data and manage errors

--- src/server/src/api/matomo.js
+++ src/server/src/api/matomo.js
@@ -3,56 +3,96 @@ import qs from "query-string";
 
 import withAuth from "../middlewares/auth";
 import UsersFeedback from "../models/UsersFeedback";
-import { isDev } from "../utils/isDev";
 
 const express = require("express");
 const router = express.Router();
 
-router.get("/matomo/:date/:range", withAuth, async (req, res) => {
-  const feedback = new UsersFeedback();
+router.get("/matomo/:months/:startDate", withAuth, async (req, res) => {
+  if (!process.env.MATOMO_ID_SITE || !process.env.MATOMO_TOKEN_AUTH) {
+    console.error(
+      "WARNING MATOMO: It seems matomo config in .env file is not completed"
+    );
+  }
 
-  const params = qs.stringify({
+  const { startDate, months } = req.params;
+  const feedback = new UsersFeedback();
+  const endDate = new Date().toISOString().split("T")[0];
+
+  if (typeof startDate !== "string") {
+    console.error("'startDate' parameter must be a string");
+    return res.status(400).send({
+      success: false,
+      error: "'startDate' parameter must be a string",
+    });
+  }
+
+  if (typeof months !== "string") {
+    console.error("'months' parameter must be a string");
+    return res.status(400).send({
+      success: false,
+      error: "'months' parameter must be a string",
+    });
+  }
+
+  const statsParams = qs.stringify({
     module: "API",
     method: "API.get",
     idSite: process.env.MATOMO_ID_SITE,
-    date: req.params.date,
-    period: req.params.range,
+    date: `last${months}`,
+    period: "month",
     format: "JSON",
     token_auth: process.env.MATOMO_TOKEN_AUTH,
   });
 
-  /*
-   * //////////////////////
-   * // TODO:
-   * // A VIRER ET UTILISER 2 ENV :
-   * // NODE_TLS_REJECT_UNAUTHORIZED A ACTIVER POUR PROD ET DESACTIVER POUR DEV
-   * //////////////////////
-   */
-  if (isDev) {
-    process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
-  }
+  const usersParams = qs.stringify({
+    module: "API",
+    method: "UserId.getUsers",
+    idSite: process.env.MATOMO_ID_SITE,
+    date: `${startDate},${endDate}`,
+    period: "range",
+    format: "JSON",
+    filter_limit: "-1",
+    token_auth: process.env.MATOMO_TOKEN_AUTH,
+  });
 
   try {
-    const satisfactionRates = await feedback.getSatisfactionRates();
-    const totalRate = satisfactionRates.reduce((a, b) => {
-      return parseInt(a) + parseInt(b);
-    }, 0);
-    const averageRate = Math.round(totalRate / satisfactionRates.length);
+    const satisfactionRates = await feedback.getSatisfactionRates(startDate);
 
-    try {
-      const { data } = await axios.get(`${process.env.MATOMO_URL}?${params}`);
-      res.send({ ...data, avg_satisfaction_rate: averageRate });
-    } catch (error) {
-      console.error({ error });
-
-      res.status(error.response.status || 400).send({
-        success: false,
-        error: error.response.statusText,
-      });
+    if (!satisfactionRates) {
+      throw Error("Error: UsersFeedback get query have failed");
     }
-    throw new Error("An error has occured: Matomo data can't be fetch");
-  } catch (e) {
-    console.error("An error has occured", e);
+
+    const totalRate =
+      Array.isArray(satisfactionRates) &&
+      satisfactionRates.reduce((a, b) => {
+        return parseInt(a) + parseInt(b);
+      }, 0);
+
+    const averageRate =
+      Number.isInteger(totalRate) &&
+      Array.isArray(satisfactionRates) &&
+      Math.round(totalRate / satisfactionRates.length);
+
+    const stats = await axios.get(`${process.env.MATOMO_URL}?${statsParams}`);
+    const users = await axios.get(`${process.env.MATOMO_URL}?${usersParams}`);
+
+    res.send({
+      ...stats.data,
+      users: users.data,
+      avg_satisfaction_rate: averageRate,
+    });
+  } catch (error) {
+    console.error(
+      "Matomo data can't be fetch : ",
+      error.response.status,
+      error.response.statusText
+    );
+    res.status(error.response.status || 400).send({
+      success: false,
+      error:
+        error.response.statusText ||
+        "An error was occured: Matomo data can't be fetch",
+    });
   }
 });
 

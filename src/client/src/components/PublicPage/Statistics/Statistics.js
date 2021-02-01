92a36944d359de6dffa604c8d4d504ddfbc4a8f8
Eutrope Valentin <valentin.eutrope@free.fr>
2021-02-01 17:42:28 +0100

    feat(client: publicPage): implement graph and update stats page

--- src/client/src/components/PublicPage/Statistics/Statistics.js
+++ src/client/src/components/PublicPage/Statistics/Statistics.js
@@ -1,23 +1,30 @@
 import React, { useState, useEffect } from "react";
-import moment from "moment";
+import _map from "lodash.map";
 
+import {
+  nbUsersReturningAverage,
+  extractStats,
+  getAverageRateTimeOnSite,
+  sum
+} from "../../../helpers/Matomo/Statistics";
+import { getStartDateStatsParam } from "../../../helpers/Date";
 import Http from "../../../services/Http";
 import StatsFilters from "./StatsFilters";
 import LoadSpinner from "../../../components/shared/LoadSpinner";
 import Value from "../../../components/shared/Value";
+import VisitsGraph from "./VisitsGraph";
 import "./statistics.scss";
 
 const Statistics = () => {
   const [stats, setStats] = useState();
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
-  const [range, setRange] = useState({ label: "jour", value: "day" });
-  const [date, setDate] = useState(moment().format("YYYY-MM-DD"));
+  const [range, setRange] = useState({ label: "12 mois", value: "12" });
 
   useEffect(() => {
-    const getStats = (date, range) => {
+    const getStats = months => {
       setIsLoading(true);
-      return Http.get(`/matomo/${date}/${range}`)
+      return Http.get(`/matomo/${months}/${getStartDateStatsParam(months)}`)
         .then(res => res.data)
         .catch(error => {
           console.error(error);
@@ -26,27 +33,49 @@ const Statistics = () => {
         .finally(() => setIsLoading(false));
     };
 
-    getStats(date, range.value).then(res => setStats(res));
-  }, [date, range]);
+    getStats(range.value).then(res => {
+      const nbUsersList = _map(extractStats(res), "nb_users");
+      const nbUsersReturningList = _map(
+        extractStats(res),
+        "nb_users_returning"
+      );
 
-  const usersReturningRate =
-    stats &&
-    parseFloat((100 * (stats.nb_users_returning / stats.nb_users)).toFixed(1));
+      setStats({
+        nb_users: res?.users.length,
+        nb_users_returning_average: nbUsersReturningAverage(
+          nbUsersList,
+          nbUsersReturningList
+        ),
+        nb_visits: sum(_map(extractStats(res), "nb_visits")),
+        avg_time_on_site: getAverageRateTimeOnSite(
+          _map(extractStats(res), "avg_time_on_site"),
+          range.value
+        ),
+        nb_pageviews: sum(_map(extractStats(res), "nb_pageviews")),
+        avg_satisfaction_rate: res?.avg_satisfaction_rate,
+        graph_visits_data: _map(extractStats(res), (monthData, date) => {
+          return { label: date, value: monthData.nb_visits };
+        })
+      });
+    });
+  }, [range]);
 
   const data = [
     { label: "Nombre d'utilisateurs", value: stats?.nb_users },
     {
       label: "Part des utilisateurs de retour sur le site",
-      value: isNaN(usersReturningRate) ? "-" : usersReturningRate + "%"
+      value: isNaN(stats?.nb_users_returning_average)
+        ? "-"
+        : stats?.nb_users_returning_average + "%"
     },
     {
       label: "Niveau de satisfaction des utilisateurs",
-      value: stats?.avg_satisfaction_rate + " / 10"
+      value: stats?.avg_satisfaction_rate || "-" + " / 10"
     },
     { label: "Nombre de visite sur le site", value: stats?.nb_visits },
     {
       label: "Durée moyenne d’une visite",
-      value: stats?.avg_time_on_site ? stats.avg_time_on_site + "s" : "-"
+      value: stats?.avg_time_on_site + " min"
     },
     {
       label: "Nombre de pages consultées",
@@ -56,20 +85,19 @@ const Statistics = () => {
 
   return (
     <div className="page content">
-      <h1>Statistiques Matomo</h1>
+      <h1>Statistiques</h1>
       <StatsFilters
         setRange={setRange}
         range={range}
-        setDate={setDate}
-        date={date}
         setStats={setStats}
+        isLoading={isLoading}
       />
-      {error ? (
+      {error && (
         <div className="error">
-          Une erreur est survenue, les données Matomo ne peuvent pas être
-          récupérées
+          Une erreur est survenue lors de la récupération des données Matomo
         </div>
-      ) : isLoading ? (
+      )}
+      {isLoading ? (
         <LoadSpinner />
       ) : (
         <div className="stats">
@@ -84,6 +112,9 @@ const Statistics = () => {
             ))}
           </div>
         </div>
+      )}
+      {stats?.graph_visits_data && (
+        <VisitsGraph data={stats.graph_visits_data} />
       )}
     </div>
   );
--- src/client/src/components/PublicPage/Statistics/StatsFilters.js
+++ src/client/src/components/PublicPage/Statistics/StatsFilters.js
@@ -1,35 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
-import moment from "moment";
 import Select from "react-select";
 
 import { selectCustomStyles } from "../../../components/Search/Filters/customStyles";
 import "./statistics.scss";
 
-const StatsFilters = ({ setDate, date, range, setRange }) => {
+const StatsFilters = ({ range, setRange, isLoading }) => {
   const options = [
-    { label: "jour", value: "day" },
-    { label: "semaine", value: "week" },
-    { label: "mois", value: "month" },
-    { label: "année", value: "year" }
+    { label: "1 mois", value: "1" },
+    { label: "3 mois", value: "3" },
+    { label: "6 mois", value: "6" },
+    { label: "12 mois", value: "12" }
   ];
 
   return (
     <div className="stats-filters">
-      <input
-        className="datepicker"
-        type="date"
-        id="date"
-        name="date"
-        value={date}
-        max={moment().format("YYYY-MM-DD")}
-        onChange={e => setDate(moment(e.target.value).format("YYYY-MM-DD"))}
-      />
       <Select
         id="range"
         name="range"
         options={options}
         value={range}
+        isDisabled={isLoading}
         onChange={range => range && setRange(range)}
         placeholder="Période"
         styles={selectCustomStyles}
@@ -39,15 +30,14 @@ const StatsFilters = ({ setDate, date, range, setRange }) => {
 };
 
 StatsFilters.propTypes = {
-  date: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired,
   range: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.shape({
       value: PropTypes.string
     })
   ]).isRequired,
-  setDate: PropTypes.func.isRequired,
-  setRange: PropTypes.func.isRequired
+  setRange: PropTypes.func.isRequired,
+  isLoading: PropTypes.bool.isRequired
 };
 
 export default StatsFilters;

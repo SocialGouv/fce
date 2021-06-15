608c38039c4e9e5447bc9ef0cdf82373dc0565e1
Clément Berthou <clement.berthou@benextcompany.com>
2021-06-15 11:24:34 +0200

    add k8s

---
+++ .k8s/components/server.ts
@@ -0,0 +1,31 @@
+import env from "@kosko/env";
+
+import { create } from "@socialgouv/kosko-charts/components/app";
+
+const tag = process.env.CI_COMMIT_TAG
+  ? process.env.CI_COMMIT_TAG.slice(1)
+  : process.env.CI_COMMIT_SHA;
+
+const manifests = create("app", {
+  env,
+  config: {
+    containerPort: 80,
+  },
+  deployment: {
+    image: `ghcr.io/socialgouv/fce/server:sha-${tag}`,
+    container: {
+      resources: {
+        requests: {
+          cpu: "100m",
+          memory: "128Mi",
+        },
+        limits: {
+          cpu: "500m",
+          memory: "1280Mi",
+        },
+      },
+    },
+  },
+});
+
+export default manifests;

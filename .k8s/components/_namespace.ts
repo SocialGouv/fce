14e94146f4f52824187d0c101cda44dc46f5f6cb
Clément Berthou <clement.berthou@benextcompany.com>
2021-06-21 18:23:34 +0200

    fix: fix namespace and kubeconfig

--- .k8s/components/_namespace.ts
+++ .k8s/components/_namespace.ts
@@ -2,4 +2,12 @@ import { createNamespace } from "@socialgouv/kosko-charts/components/namespace";
 
 const manifests = createNamespace();
 
+manifests.metadata = {
+  ...manifests.metadata,
+  annotations: {
+    ...manifests.metadata?.annotations,
+    "field.cattle.io/projectId": process.env.RANCHER_PROJECT_ID || ""
+  }
+}
+
 export default manifests;

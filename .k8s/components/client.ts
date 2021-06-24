39ec26f9d8e538136067b93329f7fdde91ffb5d9
Clément Berthou <clement.berthou@benextcompany.com>
2021-06-24 12:17:00 +0200

    feat: add dynamic server url

--- .k8s/components/client.ts
+++ .k8s/components/client.ts
@@ -1,6 +1,11 @@
 import env from "@kosko/env";
+import serverManifest from "./server"
+import { ok } from "assert";
 
 import { create } from "@socialgouv/kosko-charts/components/app";
+import { EnvVar } from "kubernetes-models/v1";
+import {addEnv, getIngressHost, getManifestByKind} from "@socialgouv/kosko-charts/utils";
+import { Deployment } from "kubernetes-models/apps/v1/Deployment";
 
 const tag = process.env.SHA;
 
@@ -24,6 +29,23 @@ const manifests = create("client", {
       },
     },
   },
+});
+
+const serverUrl = new EnvVar({
+  name: "SERVER_URL",
+  value: `https://${getIngressHost(serverManifest)}/`,
+});
+
+//@ts-expect-error
+const deployment = getManifestByKind(manifests, Deployment) as Deployment;
+
+ok(deployment);
+
+addEnv({
+  //@ts-expect-error
+  deployment,
+  //@ts-expect-error
+  data: serverUrl
 });
 
 export default manifests;

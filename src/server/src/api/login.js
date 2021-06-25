681218daab2d9ace544840b3810e3c618073f635
Clément Berthou <clement.berthou@benextcompany.com>
2021-06-25 14:16:26 +0200

    fix

--- src/server/src/api/login.js
+++ src/server/src/api/login.js
@@ -20,7 +20,7 @@ router.post("/requestAuthCode", async (req, res) => {
       throw new Error("Connexion refusée");
     }
 
-    const code = Auth.generateCode(email);
+    const code = await Auth.generateCode(email);
 
     if (!code) {
       throw new Error("La génération du code a échouée");

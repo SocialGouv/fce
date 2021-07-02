129c5bfaddba7ce9cee853503bd2b588db4ed709
Clément Berthou <cberthou@users.noreply.github.com>
2021-07-02 17:02:28 +0200

    fix: update kosko-charts version (#16)

--- .k8s/__tests__/prod.ts
+++ .k8s/__tests__/prod.ts
@@ -5,8 +5,13 @@ import { project } from "@socialgouv/kosko-charts/testing/fake/gitlab-ci.env";
 
 jest.setTimeout(1000 * 60);
 test("kosko generate --prod", async () => {
-  process.env.IS_PROD = "true";
   expect(
-    await getEnvManifests("prod", "", project("fce").prod)
-  ).toMatchSnapshot();
+    await getEnvManifests("prod", "", {
+      ...project("fce").prod,
+      CI_COMMIT_TAG: "",
+      CI_COMMIT_SHA: "",
+      GITHUB_REF: "v1.2.3",
+      GITHUB_SHA: "0123456789abcdefghijklmnopqrstuvwxyz0123",
+      RANCHER_PROJECT_ID: "c-gjtkk:p-8j5hf",
+  })).toMatchSnapshot();
 });
--- .k8s/components/client.ts
+++ .k8s/components/client.ts
@@ -1,58 +1,62 @@
 import env from "@kosko/env";
 import { create } from "@socialgouv/kosko-charts/components/app";
-import {getManifestByKind} from "@socialgouv/kosko-charts/utils";
+import { getManifestByKind } from "@socialgouv/kosko-charts/utils";
 import { Ingress } from "kubernetes-models/networking.k8s.io/v1/Ingress";
-import {getGithubRegistryImagePath} from "@socialgouv/kosko-charts/utils/getGithubRegistryImagePath";
+import {getGithubRegistryImagePath} from "../utils/getGithubRegistryImagePath";
 
 const project = "fce";
 const name = "client";
 
-const manifests = create(name, {
-  env,
-  config: {
-    containerPort: 80,
-  },
-  deployment: {
-    image: getGithubRegistryImagePath({ name, project }),
-    container: {
-      resources: {
-        requests: {
-          cpu: "100m",
-          memory: "128Mi",
-        },
-        limits: {
-          cpu: "500m",
-          memory: "1280Mi",
+const createManifests = async () => {
+  const manifests = await create(name, {
+    env,
+    config: {
+      containerPort: 80,
+    },
+    deployment: {
+      image: getGithubRegistryImagePath({ name, project }),
+      container: {
+        resources: {
+          requests: {
+            cpu: "100m",
+            memory: "128Mi",
+          },
+          limits: {
+            cpu: "500m",
+            memory: "1280Mi",
+          },
         },
       },
     },
-  },
-});
+  });
 
-//@ts-expect-error
-const ingress = getManifestByKind(manifests, Ingress) as Ingress;
+  //@ts-expect-error
+  const ingress = getManifestByKind(manifests, Ingress) as Ingress;
 
-//@ts-expect-error
-ingress.spec?.rules[0].http.paths.push({
-  path: "/api",
-  pathType: "Prefix",
-  backend: {
-    //@ts-expect-error
-    serviceName: "server",
-    servicePort: 80,
-  }
-})
+  //@ts-expect-error
+  ingress.spec?.rules[0].http.paths.push({
+    path: "/api",
+    pathType: "Prefix",
+    backend: {
+      //@ts-expect-error
+      serviceName: "server",
+      servicePort: 80,
+    }
+  })
 
-ingress.spec?.rules?.forEach(rule => {
-  if (process.env.IS_PROD === "true") {
-    rule.host = `new-${rule.host}`
-  }
-});
+  ingress.spec?.rules?.forEach(rule => {
+    if (process.env.IS_PROD === "true") {
+      rule.host = `new-${rule.host}`
+    }
+  });
 
-ingress.spec?.tls?.forEach((tls => {
-  if (process.env.IS_PROD === "true") {
-    tls.hosts = tls.hosts?.map((host) => `new-${host}`)
-  }
-}))
+  ingress.spec?.tls?.forEach((tls => {
+    if (process.env.IS_PROD === "true") {
+      tls.hosts = tls.hosts?.map((host) => `new-${host}`)
+    }
+  }))
 
-export default manifests;
+  return manifests;
+}
+
+export default createManifests;

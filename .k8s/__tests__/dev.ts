89158addc4d51128a256a42859f5370c290be43b
Clément Berthou <clement.berthou@benextcompany.com>
2021-07-02 17:28:29 +0200

    fix: fix prod namespace

--- .k8s/__tests__/dev.ts
+++ .k8s/__tests__/dev.ts
@@ -10,7 +10,6 @@ test("kosko generate --dev", async () => {
       ...project("fce").dev,
       CI_COMMIT_TAG: "",
       CI_COMMIT_SHA: "",
-      GITHUB_REF: "v1.3.2",
       GITHUB_SHA: "0123456789abcdefghijklmnopqrstuvwxyz0123",
       RANCHER_PROJECT_ID: "c-gjtkk:p-8j5hf",
     })

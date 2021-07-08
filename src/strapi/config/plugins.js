0e5389f17d4fa24824889691abb0312c5e95cb4e
Clément Berthou <cberthou@users.noreply.github.com>
2021-07-08 12:27:05 +0200

    feat: add strapi to repo and k8s (#19)
    * feat: add strapi to repo and k8s
    
    * fix: fix strapi url
    
    * fix: fix strapi subdomain
    
    * fix: add admin JWT secret
    
    * fix: fix strapi asset displays and add prod conf
    
    * fix: fix k8s tests

---
+++ src/strapi/config/plugins.js
@@ -0,0 +1,14 @@
+module.exports = ({ env }) => ({
+  upload: {
+    provider: 'azure-storage',
+    providerOptions: {
+      account: env('STORAGE_ACCOUNT'),
+      accountKey: env('STORAGE_ACCOUNT_KEY'),
+      serviceBaseURL: env('STORAGE_URL'),
+      containerName: env('STORAGE_CONTAINER_NAME'),
+      cdnBaseURL: env('STORAGE_CDN_URL'),
+      defaultPath: 'assets',
+      maxConcurrent: 10
+    }
+  }
+});

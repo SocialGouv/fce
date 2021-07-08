bd406a8cad9438c8b1b114a1d76a7a463cbc84da
Clément Berthou <clement.berthou@benextcompany.com>
2021-07-08 13:08:37 +0200

    fix: fix strapi image prod build

--- .k8s/components/strapi.ts
+++ .k8s/components/strapi.ts
@@ -3,8 +3,6 @@ import env from "@kosko/env";
 import { create } from "@socialgouv/kosko-charts/components/app";
 import { getGithubRegistryImagePath } from "../utils/getGithubRegistryImagePath";
 import { Probe } from "kubernetes-models/v1";
-import {getManifestByKind} from "@socialgouv/kosko-charts/utils";
-import {Ingress} from "kubernetes-models/networking.k8s.io/v1/Ingress";
 
 const project = "fce";
 const name = "strapi";
@@ -17,46 +15,34 @@ const probe = new Probe({
   initialDelaySeconds: 30,
 });
 
-const manifests = create(name, {
-  env,
-  config: {
-    containerPort: 1337,
-    subDomainPrefix: "strapi-",
-  },
-  deployment: {
-    image: getGithubRegistryImagePath(({ project, name })),
-    container: {
-      livenessProbe: probe,
-      readinessProbe: probe,
-      startupProbe: probe,
-      resources: {
-        requests: {
-          cpu: "100m",
-          memory: "128Mi",
-        },
-        limits: {
-          cpu: "500m",
-          memory: "1280Mi",
+const createManifests = async () => {
+  const manifests = await create(name, {
+    env,
+    config: {
+      containerPort: 1337,
+      subDomainPrefix: process.env.SOCIALGOUV_PRODUCTION ? "strapi-new-" : "strapi-",
+    },
+    deployment: {
+      image: getGithubRegistryImagePath(({ project, name })),
+      container: {
+        livenessProbe: probe,
+        readinessProbe: probe,
+        startupProbe: probe,
+        resources: {
+          requests: {
+            cpu: "100m",
+            memory: "128Mi",
+          },
+          limits: {
+            cpu: "500m",
+            memory: "1280Mi",
+          },
         },
       },
     },
-  },
-});
+  });
 
+  return manifests;
+}
 
-//@ts-expect-error
-const ingress = getManifestByKind(manifests, Ingress) as Ingress;
-
-ingress.spec?.rules?.forEach(rule => {
-  if (process.env.SOCIALGOUV_PRODUCTION === "true") {
-    rule.host = `new-${rule.host}`
-  }
-});
-
-ingress.spec?.tls?.forEach((tls => {
-  if (process.env.SOCIALGOUV_PRODUCTION === "true") {
-    tls.hosts = tls.hosts?.map((host) => `new-${host}`)
-  }
-}))
-
-export default manifests;
+export default createManifests;

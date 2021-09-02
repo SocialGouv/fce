16614e0b5af7ee149b25643c4b5657366ee5c9fc
Clément Berthou <clement.berthou@benextcompany.com>
2021-09-02 16:07:38 +0200

    feat(n8n): add archive minio node

---
+++ src/n8n/clients/minio.ts
@@ -0,0 +1,14 @@
+import { IExecuteFunctions } from "n8n-core";
+import { Client, ClientOptions } from "minio";
+
+export const createMinioClient = (context: IExecuteFunctions) => {
+  const minioCredentials = context.getCredentials("minio") as unknown as ClientOptions;
+
+  const sanitizedCredentials = {
+    ...minioCredentials,
+    port: Number(minioCredentials.port),
+    useSSL: Boolean(minioCredentials.useSSL)
+  };
+
+  return new Client(sanitizedCredentials);
+}

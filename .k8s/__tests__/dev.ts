c9ce079ff4d5c254888b7ff5b98d753b3a8da1d8
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2021-09-14 14:57:48 +0200

    fix: Upgrade kosko-charts.

--- .k8s/__tests__/dev.ts
+++ .k8s/__tests__/dev.ts
@@ -8,8 +8,6 @@ test("kosko generate --dev", async () => {
   expect(
     await getEnvManifests("dev", "", {
       ...project("fce").dev,
-      CI_COMMIT_TAG: "",
-      CI_COMMIT_SHA: "",
       GITHUB_SHA: "0123456789abcdefghijklmnopqrstuvwxyz0123",
       RANCHER_PROJECT_ID: "c-gjtkk:p-8j5hf",
     })

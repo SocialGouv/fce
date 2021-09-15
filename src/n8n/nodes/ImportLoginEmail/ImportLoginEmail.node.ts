188ef5e014943526c6c53530570dfa3bfb404f7b
Clément Berthou <clement.berthou@benextcompany.com>
2021-09-15 11:54:34 +0200

    feat(n8n): handle wrong extensions in provided emails

--- src/n8n/nodes/ImportLoginEmail/ImportLoginEmail.node.ts
+++ src/n8n/nodes/ImportLoginEmail/ImportLoginEmail.node.ts
@@ -2,13 +2,37 @@ import { INodeExecutionData, INodeType, INodeTypeDescription } from "n8n-workflo
 import { IExecuteFunctions } from "n8n-core";
 import { ingestDb, IngestDbConfig } from "../../utils/ingestDb";
 import { mapRow } from "../../utils/postgre";
+import {getDepartementDomainFromCode} from "../../utils/departement";
+import {urlencoded} from "express";
 
 type UserData = {
   email: string;
   structure: string;
 }
 
-const config: IngestDbConfig = {
+const replaceEmailDomain = (email: string, codeDepartement: string): string => {
+  const splitEmail = email.split("@");
+  splitEmail[1] = `${getDepartementDomainFromCode(codeDepartement)}.gouv.fr`;
+  return splitEmail.join("@");
+}
+
+const trimStructure = ({ structure, ...rest }: UserData): UserData => ({
+  ...rest,
+  structure: structure.substr(0, 10)
+});
+
+const fixEmail = (codeDepartement: string) => ({ email, ...rest }: UserData): UserData => ({
+  ...rest,
+  email: email.includes("@sante.melanie2.i2") ? replaceEmailDomain(email, codeDepartement) : email,
+})
+
+const sanitizeUserData = (codeDepartement: string | null) => (userData: UserData): UserData => {
+  const emailProcess = codeDepartement ? fixEmail(codeDepartement) : (userData: UserData) => userData;
+
+  return emailProcess(trimStructure(userData));
+}
+
+const getConfig = (codeDepartement: string | null): IngestDbConfig => ({
   fieldsMapping: {
     "Courriel": "email",
     "Structure": "structure"
@@ -19,12 +43,9 @@ const config: IngestDbConfig = {
   nonEmptyFields: ["email"],
   separator: ",",
   transform: () => mapRow<UserData, UserData>(
-    ({ structure, ...rest }) => ({
-      ...rest,
-      structure: structure.substr(0, 9)
-    })
+    sanitizeUserData(codeDepartement)
   )
-};
+});
 
 export class ImportLoginEmail implements INodeType {
   description: INodeTypeDescription = {
@@ -37,16 +58,35 @@ export class ImportLoginEmail implements INodeType {
       name: 'Import Login Email',
       color: '#772244',
     },
+    properties: [
+      // Node properties which the user gets displayed and
+      // can change on the node.
+      {
+        displayName: 'Remote file name',
+        name: 'remoteFile',
+        type: 'string',
+        default: '',
+        placeholder: 'name',
+        description: 'The name of the remote file from which we extract the departement code',
+        required: true
+      }
+    ],
     credentials: [{
       name: "postgres",
       required: true
     }],
     inputs: ['main'],
     outputs: ['main'],
-    properties: []
   };
 
   async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
-    return ingestDb(this, config);
+    const remoteFileName = this.getNodeParameter("remoteFile", 0) as string;
+    const extractDepartmentRegex = /^Annuaire-DDETS(PP)? ([0-9A-z]{1,2}).csv$/;
+
+    const extractResult = extractDepartmentRegex.exec(remoteFileName);
+
+    const codeDepartment = extractResult && extractResult[2];
+
+    return ingestDb(this, getConfig(codeDepartment));
   }
 }

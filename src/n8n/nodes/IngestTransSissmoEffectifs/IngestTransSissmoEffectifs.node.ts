b61d052518e3649aad0785c37854c70e59c2574c
Clément Berthou <clement.berthou@benextcompany.com>
2021-09-23 15:41:02 +0200

    fix(n8n): add etablissement current state import

--- src/n8n/nodes/IngestTransSissmoEffectifs/IngestTransSissmoEffectifs.node.ts
+++ src/n8n/nodes/IngestTransSissmoEffectifs/IngestTransSissmoEffectifs.node.ts
@@ -2,7 +2,11 @@ import { INodeExecutionData, INodeType, INodeTypeDescription } from "n8n-workflo
 import { IExecuteFunctions } from "n8n-core";
 import { ingestDb, IngestDbConfig } from "../../utils/ingestDb";
 
-const config: IngestDbConfig = {
+type ConfigOptions = {
+  isLatestUpdate: boolean,
+};
+
+const config = ({ isLatestUpdate }: ConfigOptions): IngestDbConfig => ({
   fieldsMapping: {
     "MOIS": "mois",
     "SIRET": "siret",
@@ -17,8 +21,8 @@ const config: IngestDbConfig = {
     "DATE_MAJ": "date_maj",
   },
   filename: "ts_effectifs.csv",
-  table: "etablissements_dsn_effectif",
-  truncate: false,
+  table: isLatestUpdate ? "etablissements_dsn_eff" : "etablissements_dsn_effectif",
+  truncate: isLatestUpdate,
   separator: ",",
   date: {
     field: "date_maj",
@@ -27,7 +31,7 @@ const config: IngestDbConfig = {
   padSiret: true,
   nonEmptyFields: ["siret"],
   bypassConflictSafeInsert: true,
-};
+});
 
 export class IngestTransSissmoEffectifs implements INodeType {
   description: INodeTypeDescription = {
@@ -46,10 +50,19 @@ export class IngestTransSissmoEffectifs implements INodeType {
     }],
     inputs: ['main'],
     outputs: ['main'],
-    properties: []
+    properties: [{
+      displayName: 'Latest update',
+      name: 'latestUpdate',
+      type: 'boolean',
+      default: false,
+      description: 'Check if the ingest is the latest file',
+    }]
   };
 
   async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
-    return ingestDb(this, config);
+    const isLatestUpdate = this.getNodeParameter("latestUpdate", 0, false) as boolean;
+    return ingestDb(this, config({
+      isLatestUpdate
+    }));
   }
 }

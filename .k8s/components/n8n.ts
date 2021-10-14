50449c14bc13fc18fc177784feb9505aa76eff51
Clément Berthou <clement.berthou@benextcompany.com>
2021-10-14 11:09:20 +0200

    fix(n8n): fix n8n preprod deployment

--- .k8s/components/n8n.ts
+++ .k8s/components/n8n.ts
@@ -4,7 +4,6 @@ import env from "@kosko/env";
 import { create } from "@socialgouv/kosko-charts/components/app";
 import { getGithubRegistryImagePath } from "../utils/getGithubRegistryImagePath";
 import {Probe, Volume, VolumeMount} from "kubernetes-models/v1";
-import {azureProjectVolume} from "@socialgouv/kosko-charts/components/azure-storage/azureProjectVolume";
 
 const project = "fce";
 const name = "n8n";
@@ -16,20 +15,6 @@ const downloadsVolume = new Volume({
 const downloadsVolumeMount = new VolumeMount({
   name: "downloads",
   mountPath: "/tmp/download"
-});
-
-const [persistentVolumeClaim, persistentVolume] = azureProjectVolume("n8n-db", {
-  storage: "1Gi"
-});
-
-const dbVolume = new Volume({
-  name: "n8n-db",
-  persistentVolumeClaim: { claimName: persistentVolumeClaim.metadata!.name! },
-});
-
-const dbVolumeMount = new VolumeMount({
-  mountPath: "/home/node/.n8n",
-  name: "n8n-db",
 });
 
 const probe = new Probe({
@@ -49,7 +34,7 @@ const createManifests = async () => {
     },
     deployment: {
       image: getGithubRegistryImagePath(({ project, name })),
-      volumes: [downloadsVolume, dbVolume],
+      volumes: [downloadsVolume],
       container: {
         livenessProbe: probe,
         readinessProbe: probe,
@@ -64,78 +49,12 @@ const createManifests = async () => {
             memory: "1280Mi",
           },
         },
-        volumeMounts: [downloadsVolumeMount, dbVolumeMount]
+        volumeMounts: [downloadsVolumeMount]
       },
     },
   });
 
-  return [...manifests, persistentVolumeClaim, persistentVolume];
+  return [...manifests];
 }
 
 export default createManifests;
-
-// import env from "@kosko/env";
-
-// import { create } from "@socialgouv/kosko-charts/components/app";
-// import { getGithubRegistryImagePath } from "../utils/getGithubRegistryImagePath";
-// import { Probe } from "kubernetes-models/v1";
-
-// const project = "fce";
-// const name = "n8n";
-
-// const probe = new Probe({
-//   httpGet: {
-//     path: "/healthz",
-//     port: "http",
-//   },
-//   initialDelaySeconds: 60,
-// });
-
-// const createManifests = async () => {
-//   const manifests = await create(name, {
-//     env,
-//     config: {
-//       containerPort: 5678,
-//       subDomainPrefix: "n8n-",
-//     },
-//     deployment: {
-//       image: getGithubRegistryImagePath(({ project, name })),
-//       volumes: [{
-//         name: "downloads"
-//       }, {
-//         name: "n8n-db",
-//         azureFile: {
-//           readOnly: false,
-//           secretName: "azure-fce-volume",
-//           shareName: "n8n-db",
-//         }
-//       }],
-//       container: {
-//         livenessProbe: probe,
-//         readinessProbe: probe,
-//         startupProbe: probe,
-//         resources: {
-//           requests: {
-//             cpu: "50m",
-//             memory: "128Mi",
-//           },
-//           limits: {
-//             cpu: "500m",
-//             memory: "1280Mi",
-//           },
-//         },
-//         volumeMounts: [{
-//           mountPath: "/tmp/download",
-//           name: "downloads",
-//         }, {
-//           mountPath: "/home/node/.n8n",
-//           name: "n8n-db",
-//         }]
-//       },
-//     },
-//   });
-
-//   return [...manifests];
-// }
-
-// export default createManifests;

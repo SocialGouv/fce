e6784bb548ae8b9c6cf7f1d24c4a7f31980e31a0
Clément Berthou <cberthou@users.noreply.github.com>
2021-10-28 14:33:53 +0200

    fix(egapro): fix display and info message (#102)

--- src/client/src/components/DataSheets/Sections/Enterprise/EnterpriseRelationship/Egapro.js
+++ src/client/src/components/DataSheets/Sections/Enterprise/EnterpriseRelationship/Egapro.js
@@ -1,49 +1,62 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { connect } from "react-redux";
 import Subcategory from "../../SharedComponents/Subcategory";
 import PgApiDataHandler from "../../SharedComponents/PgApiDataHandler";
 import "./psi.scss";
 import Value from "../../../../shared/Value";
 import Table from "../../SharedComponents/Table";
 
-const Egapro = ({ egapro, sources }) => (
+const Egapro = ({ egapro }) => (
   <div>
     <Subcategory
       subtitle="Index de l'égalité professionnelle"
       sourceSi="Egapro"
     >
-      <PgApiDataHandler isLoading={egapro.isLoading} error={egapro.error}>
-        {egapro.index?.length > 0 ?
-          <Table className="enterprise-mandataires">
-            <thead>
-              <tr>
-                <th className="th">Année</th>
-                <th className="th">Index</th>
-              </tr>
-            </thead>
-            <tbody>
-              {egapro.index?.map(({ annee, index }) => (
-                <tr key={annee}>
-                  <td>
-                    <Value value={annee} />
-                  </td>
-                  <td>
-                    <Value value={index ? `${index}/100` : ""} empty="L'index n'a pas pu être calculé par l'entreprise  par manque de données" />
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </Table> :
-          <div className="section-datas__list-description">Aucune déclaration pour cette entreprise.</div>}
-      </PgApiDataHandler>
+      <div className="section-datas__list">
+        <div className="section-datas__list-description">
+          Toutes les entreprises d'au moins 50 salariés doivent calculer et publier leur index de l'égalité professionnelle
+          entre les hommes et les femmes, chaque année au 1er mars
+        </div>
+        <div className="section-datas__list-item">
+
+          <PgApiDataHandler isLoading={egapro.isLoading} error={egapro.error}>
+            {egapro.index?.length > 0 ? (
+              <Table className="enterprise-mandataires">
+                <thead>
+                  <tr>
+                    <th className="th">Année</th>
+                    <th className="th">Index</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {egapro.index?.map(({ annee, index }) => (
+                    <tr key={annee}>
+                      <td>
+                        <Value value={annee} />
+                      </td>
+                      <td>
+                        <Value
+                          value={index ? `${index}/100` : ""}
+                          empty="L'index n'a pas pu être calculé par l'entreprise  par manque de données"
+                        />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            ) : (
+              <div className="dt">Aucune déclaration pour cette entreprise.</div>
+            )}
+          </PgApiDataHandler>
+        </div>
+      </div>
     </Subcategory>
   </div>
 );
 
 Egapro.propTypes = {
   egapro: PropTypes.object.isRequired,
-  sources: PropTypes.object.isRequired,
+  sources: PropTypes.object.isRequired
 };
 
 export default Egapro;

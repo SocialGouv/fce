c98370b0904530fefcab7b94980f3d6bef8801f7
Adrien Risser <risseraka@gmail.com>
2022-01-24 11:59:54 +0100

    feat(of): add organismes formation #134

---
+++ src/client/src/services/OrganismeFormation/hooks.js
@@ -0,0 +1,55 @@
+import { useQuery, gql } from "@apollo/client";
+
+const organismeFormationBySiretQuery = gql`
+  query GetOrganismeFormation($siret: String!) {
+    accidents_travail(where: { siret: { _eq: $siret } }) {
+      siren
+      numero_declaration_activite
+      denomination
+      actions_de_formation
+      bilans_de_competences
+      vae
+      actions_de_formation_par_apprentissage
+      organisme_etranger_represente_denomination
+      code_specialite_1
+      libelle_specialite_1
+      code_specialite_2
+      libelle_specialite_2
+      code_specialite_3
+      libelle_specialite_3
+      nb_stagiaires
+      nb_stagiaires_confies_par_un_autre_of
+      effectif_formateurs
+    }
+  }
+`;
+
+const organismeFormationBySirenQuery = gql`
+  query GetOrganismeFormation($siren: String!) {
+    accidents_travail(where: { siren: { _eq: $siren } }) {
+      siret
+      numero_declaration_activite
+      denomination
+      actions_de_formation
+      bilans_de_competences
+      vae
+      actions_de_formation_par_apprentissage
+      organisme_etranger_represente_denomination
+      code_specialite_1
+      libelle_specialite_1
+      code_specialite_2
+      libelle_specialite_2
+      code_specialite_3
+      libelle_specialite_3
+      nb_stagiaires
+      nb_stagiaires_confies_par_un_autre_of
+      effectif_formateurs
+    }
+  }
+`;
+
+export const useOrganismeFormationBySiren = (siren) =>
+  useQuery(organismeFormationBySirenQuery, { variables: { siren } });
+
+export const useOrganismeFormationBySiret = (siret) =>
+  useQuery(organismeFormationBySiretQuery, { variables: { siret } });
---
+++ src/server/migrations/1635165709615_add-organismes-formation.js
@@ -0,0 +1,48 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable("organismes_formation", {
+    siret: {
+      type: "varchar(15)",
+      notNull: true,
+    },
+    siren: {
+      type: "varchar(9)",
+      notNull: true,
+    },
+    numero_declaration_activite: {
+      type: "varchar(11)",
+      notNull: true,
+    },
+    denomination: {
+      type: "varchar(255)",
+      notNull: true,
+    },
+    actions_de_formation: "bool",
+    bilans_de_competences: "bool",
+    vae: "bool",
+    actions_de_formation_par_apprentissage: "bool",
+    organisme_etranger_represente_denomination: "varchar(255)",
+    code_specialite_1: "varchar(3)",
+    libelle_specialite_1: "varchar(255)",
+    code_specialite_2: "varchar(3)",
+    libelle_specialite_2: "varchar(255)",
+    code_specialite_3: "varchar(3)",
+    libelle_specialite_3: "varchar(255)",
+    nb_stagiaires: "integer",
+    nb_stagiaires_confies_par_un_autre_of: "integer",
+    effectif_formateurs: "integer",
+  });
+
+  pgm.createIndex("organismes_formation", "siret");
+  pgm.createIndex("organismes_formation", "siren");
+
+  pgm.sql(`INSERT INTO import_updates (fournisseur, si, "table") VALUES
+        ('DAT', 'DAT', 'organismes_formation')`);
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable("organismes_formation");
+};

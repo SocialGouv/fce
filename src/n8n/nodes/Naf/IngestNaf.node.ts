0a681d7a729b07bf992c4d98da316d588fff30f9
Clément Berthou <clement.berthou@benextcompany.com>
2022-02-14 16:07:57 +0100

    feat(n8n): add ingest for naf v2

--- src/n8n/nodes/Naf/IngestNaf.node.ts
+++ src/n8n/nodes/Naf/IngestNaf.node.ts
@@ -1,27 +1,34 @@
 import { INodeExecutionData, INodeType, INodeTypeDescription } from "n8n-workflow";
 import { IExecuteFunctions } from "n8n-core";
 import { ingestDb, IngestDbConfig } from "../../utils/ingestDb";
+import {mapRow} from "../../utils/postgre";
 
 const config: IngestDbConfig = {
-  filename: "cateogries_juridiques.csv",
-  table: "categorie_juridique",
+  filename: "naf.csv",
+  table: "naf",
   fieldsMapping: {
     "Code": "code",
     "Libellé": "libelle"
   },
+  addedColumns: ["recherche", "nomenclature"],
+  transform: () => mapRow((row: Record<string, string>) => ({
+    ...row,
+    recherche: true,
+    nomenclature: "NAF rév. 2"
+  })),
   truncate: true,
   separator: ","
 };
 
-export class CategoriesJuridiquesIngest implements INodeType {
+export class IngestNaf implements INodeType {
   description: INodeTypeDescription = {
-    displayName: "Categories Juridiques Ingest",
-    name: "categoriesJuridiques",
+    displayName: "NAF Ingest",
+    name: "naf",
     group: ['transform'],
     version: 1,
-    description: "Categories Juridiques Ingest",
+    description: "NAF Ingest",
     defaults: {
-      name: 'Categories Juridiques Ingest',
+      name: 'NAF Ingest',
       color: '#772244',
     },
     credentials: [{

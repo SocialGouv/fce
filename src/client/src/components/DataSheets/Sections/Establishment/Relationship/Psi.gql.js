7249043f67c305b546e713c4e789cfffe5b0b731
marius colin <marius707@hotmail.fr>
2022-06-27 14:57:09 +0100

    fix: fix PSI

--- src/client/src/components/DataSheets/Sections/Establishment/Relationship/Psi.gql.js
+++ src/client/src/components/DataSheets/Sections/Establishment/Relationship/Psi.gql.js
@@ -2,20 +2,14 @@ import { gql, useQuery } from "@apollo/client";
 import { pipe } from "lodash/fp";
 
 import { BCE_CLIENT } from "../../../../../services/GraphQL/GraphQL";
+import { getSirenFromSiret } from "../../../../../utils/establishment/establishment";
 import { mapQueryResult } from "../../../../../utils/graphql/graphql";
 
-export const PsiEntrepriseFragmentQuery = gql`
-  query getPsiEntreprise($match: String!, $siren: String!) {
-    psi_siret: fce_psi_siret(where: { SIRET: { _like: $match } }) {
-      siret: SIRET
+const psiQuery = gql`
+  query GetPsi($siret: String!, $siren: String!) {
+    psi_siret: fce_psi_siret(where: { SIRET: { _eq: $siret } }) {
       salaries_annee_courante
       salaries_annee_precedente
-      etablissement: psi_siret_siret {
-        etablissementsiege
-        etatadministratifetablissement
-        codepostaletablissement
-        libellecommuneetablissement
-      }
     }
     psi_siren: fce_psi_siren(where: { siren: { _eq: $siren } }) {
       salaries_annee_courante
@@ -31,15 +25,15 @@ export const PsiEntrepriseFragmentQuery = gql`
   }
 `;
 
-export const usePsiEntreprise = pipe(
-  (siren) =>
-    useQuery(PsiEntrepriseFragmentQuery, {
+export const usePsi = pipe(
+  (siret) =>
+    useQuery(psiQuery, {
       context: { clientName: BCE_CLIENT },
-      variables: { match: `${siren}%`, siren },
+      variables: { siren: getSirenFromSiret(siret), siret },
     }),
   mapQueryResult(({ psi_siret, psi_siren, psi_update }) => ({
     psi_siren: psi_siren[0],
-    psi_siret: psi_siret,
+    psi_siret: psi_siret[0],
     psi_update: psi_update[0],
   }))
 );

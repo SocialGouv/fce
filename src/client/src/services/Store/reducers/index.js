b2b9121d3b8aea0fa8bd8f23362a8e35b907655f
unknown <imen.chermiti@ouidou.fr>
2023-03-24 13:56:48 +0100

    chore: remove non used client files

--- src/client/src/services/Store/reducers/index.js
+++ src/client/src/services/Store/reducers/index.js
@@ -1,23 +1,9 @@
 import { combineReducers } from "redux";
 
-import agreements from "./agreements";
-import apprentissage from "./apprentissage";
-import egapro from "./egapro";
-import enterprise from "./enterprise";
-import psi from "./psi";
 import search from "./search";
-import sources from "./sources";
-import successions from "./succession";
 
 const reducers = combineReducers({
-  agreements,
-  apprentissage,
-  egapro,
-  enterprise,
-  psi,
   search,
-  sources,
-  successions,
 });
 
 export default reducers;

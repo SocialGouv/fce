d98a4d44ce9fecc034218f09ac4626b8318b806c
Imen Chermiti <imen.chermiti@ouidou.fr>
2023-03-28 18:14:02 +0200

    fix: remove unused files + unused deps

--- src/server/src/shell/import/converter/xlsxToCsv.js
+++
@@ -1,8 +0,0 @@
-const xlsx = require("xlsx");
-
-module.exports = async filename => {
-  // eslint-disable-next-line security/detect-non-literal-fs-filename
-  const workBook = xlsx.readFile(filename);
-  // eslint-disable-next-line security/detect-non-literal-fs-filename
-  return xlsx.writeFile(workBook, filename, { bookType: "csv" });
-};

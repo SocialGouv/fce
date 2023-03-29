1737c673259ca5682045c117e3e19b61664624b3
ImenOuidou <127976664+ImenOuidou@users.noreply.github.com>
2023-03-29 12:00:49 +0200

    fix: remove unused files + unused deps (#251)

--- src/server/src/shell/monthImport.sh
+++
@@ -1,6 +0,0 @@
-npx ./shell/run.js DownloadSirene
-npx ./shell/run.js ImportSirene --enterprises_filename StockUniteLegale_utf8.zip --establishments_filename StockEtablissement_utf8.zip
-rm -f StockUniteLegale_utf8.zip
-rm -f StockEtablissement_utf8.zip
-
-npx scripts/appsearchIndexer.js

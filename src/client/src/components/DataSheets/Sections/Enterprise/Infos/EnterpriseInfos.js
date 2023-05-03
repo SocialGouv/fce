2f81839d20977bb1cd631c950e159983367399c8
Imen Chermiti <127976664+ImenOuidou@users.noreply.github.com>
2023-05-03 10:46:44 +0200

    feat: work on ca finance indicator (#269)

--- src/client/src/components/DataSheets/Sections/Enterprise/Infos/EnterpriseInfos.js
+++ src/client/src/components/DataSheets/Sections/Enterprise/Infos/EnterpriseInfos.js
@@ -220,7 +220,7 @@ const EnterpriseInfos = ({ enterprise: baseEntreprise }) => {
           subtitle="Données financières"
           sourceSi={"Comptes Annuels"}
         >
-          <Finances siren={siren} />
+          <Finances siren={siren} siret={getSiret(siegeSocial)} />
         </Subcategory>
         <Subcategory
           subtitle="Mandataires sociaux"
--- src/client/src/utils/donnees-ecofi/donnees-ecofi.js
+++ src/client/src/utils/donnees-ecofi/donnees-ecofi.js
@@ -13,13 +13,16 @@ const getEBE = prop("EBE");
 const getEBIT = prop("EBIT");
 const getResult = prop("Resultat_net");
 
-const formatChiffre = (chiffreAffaire) =>
-  new Intl.NumberFormat("fr-FR", {
+const formatChiffre = (chiffreAffaire) => {
+  if (chiffreAffaire !== 0 && !chiffreAffaire) return "-";
+  if (chiffreAffaire === null) return null;
+  return new Intl.NumberFormat("fr-FR", {
     compactDisplay: "short",
     currency: "EUR",
     minimumFractionDigits: 0,
     notation: "compact",
   }).format(chiffreAffaire);
+};
 
 export const getFormattedChiffreAffaire = pipe(
   getChiffreAffaire,
@@ -31,3 +34,9 @@ export const getFormattedMargeBrute = pipe(getMargeBrute, formatChiffre);
 export const getFormattedEBE = pipe(getEBE, formatChiffre);
 export const getFormattedEBIT = pipe(getEBIT, formatChiffre);
 export const getFormattedResult = pipe(getResult, formatChiffre);
+export const sortedData = (data) =>
+  data
+    ?.sort((a, b) => {
+      return new Date(b.date_fin_exercice) - new Date(a.date_fin_exercice);
+    })
+    .slice(0, 5);

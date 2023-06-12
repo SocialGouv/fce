783e6bab091f8e1503ab310aa078facf9c166d9d
Imen Chermiti <imen.chermiti@ouidou.fr>
2023-06-12 10:57:31 +0200

    feat : optimisation elastic

--- src/server/src/utils/elastic.js
+++ src/server/src/utils/elastic.js
@@ -31,9 +31,61 @@ const codesNafLabelIndex = codesNaf.reduce(
   (map, { id, label }) => map.set(id, label),
   new Map()
 );
+const generateWordCombos = (phrase) => {
+  // Sépare la phrase en mots individuels
+  const mots = phrase.split(" ");
+
+  // Fonction récursive pour générer les combos
+  function generateCombos(index, comboCourant) {
+    // Cas de base : tous les mots ont été utilisés
+    if (index === mots.length) {
+      return [comboCourant.trim()];
+    }
+
+    // Génère tous les combos en incluant ou excluant le mot actuel
+    const combosInclus = generateCombos(
+      index + 1,
+      comboCourant + " " + mots[index]
+    );
+    const combosExclus = generateCombos(index + 1, comboCourant);
+
+    return [...combosInclus, ...combosExclus];
+  }
+
+  // Appelle la fonction récursive avec l'index initial à 0 et une chaîne vide comme combo initial
+  const combos = generateCombos(0, "");
+
+  // Ajoute des combos avec les mots dans un autre ordre
+  const reverseCombos = combos.map((combo) =>
+    combo.split(" ").reverse().join(" ")
+  );
+  combos.push(...reverseCombos);
+
+  // Élimine les doublons et la chaîne vide
+  const uniqueCombos = [...new Set(combos)].filter((combo) => combo !== "");
+
+  return uniqueCombos;
+};
 
 const makeQuery = ({ query, siege, ...filters }) => {
   const siretOrSirenQuery = query.replace(/\s/g, "");
+  const mots = generateWordCombos(query);
+  console.log(mots);
+  console.log(
+    mots.map((mot) => {
+      return {
+        multi_match: {
+          query: mot,
+          type: "phrase",
+          fields: [
+            "raisonSociale",
+            "denominationUsuelleUniteLegale",
+            "enseigneEtablissement",
+          ],
+        },
+      };
+    })
+  );
   return {
     ...(query ? { min_score: 20 } : {}),
     query: {
@@ -41,70 +93,88 @@ const makeQuery = ({ query, siege, ...filters }) => {
         filter: [
           ...(siege !== ""
             ? [
-              {
-                term: { etablissementSiege: siege === "true" },
-              },
-            ]
+                {
+                  term: { etablissementSiege: siege === "true" },
+                },
+              ]
             : []),
         ],
         must: [
           ...Object.keys(filtersFieldMap).flatMap((key) =>
             filters[key]?.length > 0
               ? [
-                {
-                  terms: { [filtersFieldMap[key]]: filters[key] },
-                },
-              ]
+                  {
+                    terms: { [filtersFieldMap[key]]: filters[key] },
+                  },
+                ]
               : []
           ),
           ...(query
             ? [
-              {
-                bool: {
-                  should: [
-                    {
-                      multi_match: {
-                        query,
-                        type: "phrase",
-                        fields: [
-                          "raisonSociale",
-                          "denominationUsuelleUniteLegale",
-                          "enseigneEtablissement"
-                        ],
-                      }
-                    },
-                    ...(siretOrSirenQuery
-                      ? [
-                        {
-                          term: {
-                            siret: {
-                              value: query.replace(/\s/g, ""),
-                              boost: 100,
-                            },
+                {
+                  bool: {
+                    should: [
+                      ...mots.map((mot) => {
+                        return {
+                          multi_match: {
+                            query: mot,
+                            type: "phrase",
+                            fields: [
+                              "raisonSociale",
+                              "denominationUsuelleUniteLegale",
+                              "enseigneEtablissement",
+                            ],
                           },
-                        },
-                        {
-                          term: {
-                            siren: {
-                              value: query.replace(/\s/g, ""),
-                              boost: 100,
+                        };
+                      }),
+                      ...(siretOrSirenQuery
+                        ? [
+                            {
+                              term: {
+                                siret: {
+                                  value: query.replace(/\s/g, ""),
+                                  boost: 100,
+                                },
+                              },
                             },
-                          },
-                        },
-                      ]
-                      : []),
-                  ],
+                            {
+                              term: {
+                                siren: {
+                                  value: query.replace(/\s/g, ""),
+                                  boost: 100,
+                                },
+                              },
+                            },
+                          ]
+                        : []),
+                    ],
+                  },
                 },
-              },
-            ]
+              ]
             : []),
         ],
         should: [
-          { rank_feature: { boost: 5, field: "trancheEffectifsUniteLegaleRank" } },
-          { rank_feature: { boost: 5, field: "etablissementsUniteLegaleRank" } },
-          { rank_feature: { boost: 5, field: "caractereEmployeurEtablissementRank" } },
+          {
+            rank_feature: {
+              boost: 5,
+              field: "trancheEffectifsUniteLegaleRank",
+            },
+          },
+          {
+            rank_feature: { boost: 5, field: "etablissementsUniteLegaleRank" },
+          },
+          {
+            rank_feature: {
+              boost: 5,
+              field: "caractereEmployeurEtablissementRank",
+            },
+          },
           { match: { etablissementSiege: { boost: 10, query: "true" } } },
-          { match: { etatAdministratifEtablissement: { boost: 10, query: "A" } } },
+          {
+            match: {
+              etatAdministratifEtablissement: { boost: 10, query: "A" },
+            },
+          },
         ],
       },
     },

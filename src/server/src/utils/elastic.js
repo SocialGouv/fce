b4b10c6f438d5b98d84207510fa4dc55181ca555
Imen Chermiti <imen.chermiti@ouidou.fr>
2023-06-14 14:40:05 +0200

    feat: optimisation elastic

--- src/server/src/utils/elastic.js
+++ src/server/src/utils/elastic.js
@@ -31,61 +31,9 @@ const codesNafLabelIndex = codesNaf.reduce(
   (map, { id, label }) => map.set(id, label),
   new Map()
 );
-const generateWordCombos = (phrase) => {
-  // Sépare la phrase en mots individuels
-  const mots = phrase.split(" ");
-
-  // Fonction récursive pour générer les combos
-  function generateCombos(index, comboCourant) {
-    // Cas de base : tous les mots ont été utilisés
-    if (index === mots.length) {
-      return [comboCourant.trim()];
-    }
-
-    // Génère tous les combos en incluant ou excluant le mot actuel
-    const combosInclus = generateCombos(
-      index + 1,
-      comboCourant + " " + mots[index]
-    );
-    const combosExclus = generateCombos(index + 1, comboCourant);
-
-    return [...combosInclus, ...combosExclus];
-  }
-
-  // Appelle la fonction récursive avec l'index initial à 0 et une chaîne vide comme combo initial
-  const combos = generateCombos(0, "");
-
-  // Ajoute des combos avec les mots dans un autre ordre
-  const reverseCombos = combos.map((combo) =>
-    combo.split(" ").reverse().join(" ")
-  );
-  combos.push(...reverseCombos);
-
-  // Élimine les doublons et la chaîne vide
-  const uniqueCombos = [...new Set(combos)].filter((combo) => combo !== "");
-
-  return uniqueCombos;
-};
 
 const makeQuery = ({ query, siege, ...filters }) => {
   const siretOrSirenQuery = query.replace(/\s/g, "");
-  const mots = generateWordCombos(query);
-  console.log(mots);
-  console.log(
-    mots.map((mot) => {
-      return {
-        multi_match: {
-          query: mot,
-          type: "phrase",
-          fields: [
-            "raisonSociale",
-            "denominationUsuelleUniteLegale",
-            "enseigneEtablissement",
-          ],
-        },
-      };
-    })
-  );
   return {
     ...(query ? { min_score: 20 } : {}),
     query: {
@@ -114,19 +62,35 @@ const makeQuery = ({ query, siege, ...filters }) => {
                 {
                   bool: {
                     should: [
-                      ...mots.map((mot) => {
-                        return {
-                          multi_match: {
-                            query: mot,
-                            type: "phrase",
-                            fields: [
-                              "raisonSociale",
-                              "denominationUsuelleUniteLegale",
-                              "enseigneEtablissement",
-                            ],
-                          },
-                        };
-                      }),
+                      {
+                        multi_match: {
+                          query,
+                          type: "phrase",
+                          fields: [
+                            "raisonSociale",
+                            "denominationUsuelleUniteLegale",
+                            "enseigneEtablissement",
+                            "prenomUniteLegale",
+                            "nomUniteLegale",
+                            "naming",
+                          ],
+                        },
+                      },
+                      {
+                        multi_match: {
+                          query,
+                          type: "phrase",
+                          fields: [
+                            "raisonSociale",
+                            "denominationUsuelleUniteLegale",
+                            "enseigneEtablissement",
+                            "prenomUniteLegale",
+                            "nomUniteLegale",
+                            "naming",
+                          ],
+                          operator: "and",
+                        },
+                      },
                       ...(siretOrSirenQuery
                         ? [
                             {

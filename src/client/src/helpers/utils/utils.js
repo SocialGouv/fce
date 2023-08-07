0a5ce53ae5a74f1c26794272c4539fcb75b34175
Imen Chermiti <imen.chermiti@ouidou.fr>
2023-08-07 11:59:42 +0200

    feat: refonte design bloc Autres etablissements pagination table

--- src/client/src/helpers/utils/utils.js
+++ src/client/src/helpers/utils/utils.js
@@ -11,13 +11,18 @@ export const capitalize = (str) =>
     .map((str) => `${str.slice(0, 1).toUpperCase()}${str.slice(1)}`)
     .join(" ");
 
-const getCodeNafLibelle = (code) =>
-  codesNafLabelIndex.get(
-    code
-      .replace(/[A-z]+$/, "")
-      .padEnd(5, "0")
-      .slice(0, 5)
-  );
+const getCodeNafLibelle = (code) => {
+  if (typeof code !== "string") {
+    throw new Error("Invalid input: code parameter must be a string");
+  }
+
+  const cleanedCode = code
+    .replace(/[A-Za-z]+$/, "")
+    .padEnd(5, "0")
+    .slice(0, 5);
+
+  return codesNafLabelIndex.get(cleanedCode);
+};
 
 const codesNafLabelIndex = codesNaf.reduce(
   (map, { id, label }) => map.set(id, label),

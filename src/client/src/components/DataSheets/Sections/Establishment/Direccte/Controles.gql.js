1ff72813c3397e5c739ac3028ce2e6e90b55fca0
Imen Chermiti <imen.chermiti@ouidou.fr>
2024-01-19 13:11:06 +0100

    feat: add search by region& add anchors

--- src/client/src/components/DataSheets/Sections/Establishment/Direccte/Controles.gql.js
+++ src/client/src/components/DataSheets/Sections/Establishment/Direccte/Controles.gql.js
@@ -4,9 +4,9 @@ import { pipe } from "lodash/fp";
 import { BCE_CLIENT } from "../../../../../services/GraphQL/GraphQL";
 import { mapQueryResult } from "../../../../../utils/graphql/graphql";
 import {
-  normalizeInteractions3E,
-  normalizeInteractions3ESRC,
-  normalizeInteractionsC,
+  // normalizeInteractions3E,
+  // normalizeInteractions3ESRC,
+  // normalizeInteractionsC,
   normalizeInteractionsT,
 } from "../../../../../utils/interactions/interactions";
 
@@ -20,43 +20,43 @@ const controlesQuery = gql`
         denominationusuelleetablissement
       }
     }
-    fce_interactions_pole_t(where: { siret: { _eq: $siret } }) {
-      date
-      intervenant
-      action_sur
-      realise_pour
-      siret
-    }
-    fce_interactions_pole_3e(where: { siret: { _eq: $siret } }) {
-      inspecteurs
-      date_visite
-      type_suivi
-      region
-      siret
-    }
-    fce_interactions_pole_3e_src(where: { siret: { _eq: $siret } }) {
-      date
-      libelle_region
-      siret
-      type_controle
-      nature_controle
-      cible_controle
-      clos
-    }
+    # fce_interactions_pole_t(where: { siret: { _eq: $siret } }) {
+    #   date
+    #   intervenant
+    #   action_sur
+    #   realise_pour
+    #   siret
+    # }
+    # fce_interactions_pole_3e(where: { siret: { _eq: $siret } }) {
+    #   inspecteurs
+    #   date_visite
+    #   type_suivi
+    #   region
+    #   siret
+    # }
+    # fce_interactions_pole_3e_src(where: { siret: { _eq: $siret } }) {
+    #   date
+    #   libelle_region
+    #   siret
+    #   type_controle
+    #   nature_controle
+    #   cible_controle
+    #   clos
+    # }
   }
 `;
 
 const normalizeResponsesToInteraction = ({
-  fce_interactions_pole_3e,
-  fce_interactions_pole_3e_src,
-  fce_interactions_pole_c,
+  // fce_interactions_pole_3e,
+  // fce_interactions_pole_3e_src,
+  // fce_interactions_pole_c,
   fce_interactions_pole_t,
 }) => ({
-  interactions_pole_3e: normalizeInteractions3E(fce_interactions_pole_3e),
-  interactions_pole_3e_src: normalizeInteractions3ESRC(
-    fce_interactions_pole_3e_src
-  ),
-  interactions_pole_c: normalizeInteractionsC(fce_interactions_pole_c),
+  // interactions_pole_3e: normalizeInteractions3E(fce_interactions_pole_3e),
+  // interactions_pole_3e_src: normalizeInteractions3ESRC(
+  //   fce_interactions_pole_3e_src
+  // ),
+  // interactions_pole_c: normalizeInteractionsC(fce_interactions_pole_c),
   interactions_pole_t: normalizeInteractionsT(fce_interactions_pole_t),
 });
 

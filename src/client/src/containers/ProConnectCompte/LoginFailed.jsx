baac685bbf735f88b32f71144630421a21368f5f
Imen Chermiti <imen.chermiti@ouidou.fr>
2025-06-11 20:51:47 +0200

    feat: update proconnect access messages

--- src/client/src/containers/ProConnectCompte/LoginFailed.jsx
+++ src/client/src/containers/ProConnectCompte/LoginFailed.jsx
@@ -6,12 +6,19 @@ import Config from "../../services/Config";
 
 export default function LoginFailed() {
   return (
-    <div className="app-search_">
+    <div className="app-search_cnx_requise">
       <div className="app-search__wrapper">
         <div className="container is-fullhd">
-          <h3>Vous n’êtes pas autorisé(e) à accéder à FCE</h3>
+          <strong> Accès non autorisé</strong>
           <p>
-            Pouvez-vous s’il vous plait nous contacter sur{" "}
+            {" "}
+            Accès non autorisé Ce portail est réservé aux agents des
+            <strong> DREETS, DDETS(PP)</strong> et <strong>DDPP.</strong>
+          </p>
+          <div>Votre profil ne vous permet pas d’y accéder.</div>
+          <div>
+            Pour toute question, vous pouvez contacter l’équipe FCE/BCE à
+            l’adresse suivante :{" "}
             <a
               className="is-link"
               href={`mailto:${Config.get("contact.mailto")}`}
@@ -19,10 +26,7 @@ export default function LoginFailed() {
               bce@travail.gouv.fr
             </a>
             , afin que nous vous activions les accès ?
-          </p>
-          <a className="is-link" href="/">
-            ← Retourner à FCE
-          </a>
+          </div>
         </div>
       </div>
     </div>

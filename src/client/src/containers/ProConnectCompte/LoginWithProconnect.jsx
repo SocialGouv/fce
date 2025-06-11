07195efe86bcf9991c1fb6ee9868ede089d43a82
Imen Chermiti <127976664+ImenOuidou@users.noreply.github.com>
2025-06-11 21:29:09 +0200

    Feat integrate pro connect btn (#377)
    * feat: add proconnect btn
    
    * feat: add proconnect btn
    
    * feat: add proconnect btn
    
    * feat: hide proconnect btn
    
    * feat: add PROCONNECT domaine verification
    
    * feat: add PROCONNECT domaine verification
    
    * feat: filter proconnect users with siret & domaine
    
    * feat: update proconnect access messages

--- src/client/src/containers/ProConnectCompte/LoginWithProconnect.jsx
+++ src/client/src/containers/ProConnectCompte/LoginWithProconnect.jsx
@@ -2,8 +2,16 @@ import React from "react";
 
 export default function LoginWithProconnect() {
   return (
-    <>
-      <h2>Tester Proconnect en prod</h2>
-    </>
+    <div className="app-search_cnx_requise">
+      <div className="app-search__wrapper">
+        <div className="container is-fullhd">
+          <strong>
+            <p>Ecran temporaire pour test</p> Pour vous connecter au portail via
+            ProConnect, veuillez cliquer sur le bouton « S’identifier avec
+            Proconnect » en haut à droite de cet écran.
+          </strong>
+        </div>
+      </div>
+    </div>
   );
 }
